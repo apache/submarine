24ff255fd74402cb6a4102796343a75fd6f2bced
Xun Liu <liuxun@apache.org>
2019-06-22 00:37:10 +0800

    [SUBMARINE-91] Integrated travis CI (#2)

--- submarine-tony/tony-azkaban/src/main/java/org/apache/submarine/tony/azkaban/TonyJobArg.java
+++ submarine-tony/tony-azkaban/src/main/java/org/apache/submarine/tony/azkaban/TonyJobArg.java
@@ -1,7 +1,17 @@
 /**
- * Copyright 2018 LinkedIn Corporation. All rights reserved. Licensed under the BSD-2 Clause license.
- * See LICENSE in the project root for license information.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License. See accompanying LICENSE file.
  */
+
 package org.apache.submarine.tony.azkaban;
 
 // Standard TonY job arguments.
--- submarine-tony/tony-core/src/test/resources/scripts/exit_0_check_env.py
+++ submarine-tony/tony-core/src/test/resources/scripts/exit_0_check_env.py
@@ -1,7 +1,17 @@
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
 #
-# Copyright 2019 LinkedIn Corporation. All rights reserved. Licensed under the BSD-2 Clause license.
-# See LICENSE in the project root for license information.
+#    http://www.apache.org/licenses/LICENSE-2.0
 #
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 """
 Copyright 2018 LinkedIn Corporation. All rights reserved. Licensed under the BSD-2 Clause license.
