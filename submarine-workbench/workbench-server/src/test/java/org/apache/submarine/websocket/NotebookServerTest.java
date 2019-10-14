bc1c2b48f825997632877bcd34ed22eaf78965b1
Zac Zhou <zhouquan@apache.org>
2019-10-14 16:05:51 +0800

    SUBMARINE-248. Add websocket interface to submarine workbench server.
    ### What is this PR for?
    Add WebSocket interface to the submarine workbench server. So that the frontend and backend can have bidirectional communications.
    
    ### What type of PR is it?
    Feature
    
    ### What is the Jira issue?
    https://issues.apache.org/jira/browse/SUBMARINE-248
    
    ### How should this be tested?
    https://travis-ci.org/yuanzac/hadoop-submarine/builds/599666968
    
    ### Questions:
    * Does the licenses files need update? No
    * Is there breaking changes for older versions? No
    * Does this needs documentation? No
    
    Author: Zac Zhou <zhouquan@apache.org>
    
    Closes #56 from yuanzac/topic/SUBMARINE-248 and squashes the following commits:
    
    5d3903a [Zac Zhou] add tests for websocket.
    731c779 [Zac Zhou] Add websocket interface

---
+++ submarine-workbench/workbench-server/src/test/java/org/apache/submarine/websocket/NotebookServerTest.java
@@ -0,0 +1,97 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.submarine.websocket;
+
+import org.apache.submarine.server.AbstractWorkbenchServerTest;
+import org.eclipse.jetty.websocket.api.Session;
+import org.eclipse.jetty.websocket.api.StatusCode;
+import org.eclipse.jetty.websocket.api.WebSocketAdapter;
+import org.eclipse.jetty.websocket.client.WebSocketClient;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.net.URI;
+import java.util.concurrent.Future;
+
+public class NotebookServerTest {
+
+  @BeforeClass
+  public static void init() throws Exception {
+    AbstractWorkbenchServerTest.startUp(
+        NotebookServerTest.class.getSimpleName());
+  }
+
+  @AfterClass
+  public static void destroy() throws Exception {
+    AbstractWorkbenchServerTest.shutDown();
+  }
+
+  @Test
+  public void testWebsocketConnection() throws Exception{
+    URI uri = URI.create(
+        AbstractWorkbenchServerTest.getWebsocketApiUrlToTest());
+    WebSocketClient client = new WebSocketClient();
+    try {
+      client.start();
+      // The socket that receives events
+      EventSocket socket = new EventSocket();
+      // Attempt Connect
+      Future<Session> fut = client.connect(socket, uri);
+      // Wait for Connect
+      Session session = fut.get();
+      // Send a message
+      session.getRemote().sendString("Hello");
+      // Close session
+      //session.close();
+      session.close(StatusCode.NORMAL, "I'm done");
+    } finally {
+      client.stop();
+    }
+  }
+
+  public class EventSocket extends WebSocketAdapter
+  {
+    @Override
+    public void onWebSocketConnect(Session sess)
+    {
+      super.onWebSocketConnect(sess);
+      System.out.println("Socket Connected: " + sess);
+    }
+
+    @Override
+    public void onWebSocketText(String message)
+    {
+      super.onWebSocketText(message);
+      System.out.println("Received TEXT message: " + message);
+    }
+
+    @Override
+    public void onWebSocketClose(int statusCode, String reason)
+    {
+      super.onWebSocketClose(statusCode, reason);
+      System.out.println("Socket Closed: [" + statusCode + "] " + reason);
+    }
+
+    @Override
+    public void onWebSocketError(Throwable cause)
+    {
+      super.onWebSocketError(cause);
+      cause.printStackTrace(System.err);
+    }
+  }
+}
