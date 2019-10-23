c67db3c86e150f84f0b75bacd86f2eab23dfffca
Wanqiang Ji <wanqiang.ji@gmail.com>
2019-10-24 00:04:49 +0800

    SUBMARINE-255. [PoC] Rename the submitter package name

--- submarine-server/server-submitter/submitter-yarnservice/src/test/java/org/apache/submarine/client/cli/yarnservice/TestYarnServiceRunJobCli.java
+++ submarine-server/server-submitter/submitter-yarnservice/src/test/java/org/apache/submarine/client/cli/yarnservice/TestYarnServiceRunJobCli.java
@@ -1,4 +1,4 @@
-/**
+/*
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
  * distributed with this work for additional information
@@ -6,14 +6,15 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- * <p>
- * http://www.apache.org/licenses/LICENSE-2.0
- * <p>
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
  */
 
 package org.apache.submarine.client.cli.yarnservice;
@@ -27,17 +28,17 @@ import org.apache.hadoop.yarn.service.api.records.Service;
 import org.apache.submarine.client.cli.param.runjob.RunJobParameters;
 import org.apache.submarine.client.cli.param.runjob.TensorFlowRunJobParameters;
 import org.apache.submarine.client.cli.runjob.RunJobCli;
-import org.apache.submarine.common.MockClientContext;
-import org.apache.submarine.common.api.TensorFlowRole;
-import org.apache.submarine.common.conf.SubmarineLogs;
-import org.apache.submarine.runtimes.common.JobSubmitter;
-import org.apache.submarine.runtimes.common.StorageKeyConstants;
-import org.apache.submarine.runtimes.common.SubmarineStorage;
-import org.apache.submarine.runtimes.yarnservice.ServiceSpecFileGenerator;
-import org.apache.submarine.runtimes.yarnservice.ServiceWrapper;
-import org.apache.submarine.runtimes.yarnservice.YarnServiceJobSubmitter;
-import org.apache.submarine.runtimes.yarnservice.tensorflow.component.TensorBoardComponent;
-import org.apache.submarine.commons.utils.ZipUtilities;
+import org.apache.submarine.commons.runtime.MockClientContext;
+import org.apache.submarine.commons.runtime.api.TensorFlowRole;
+import org.apache.submarine.commons.runtime.conf.SubmarineLogs;
+import org.apache.submarine.commons.runtime.JobSubmitter;
+import org.apache.submarine.commons.runtime.fs.StorageKeyConstants;
+import org.apache.submarine.commons.runtime.fs.SubmarineStorage;
+import org.apache.submarine.server.submitter.yarnservice.ServiceSpecFileGenerator;
+import org.apache.submarine.server.submitter.yarnservice.ServiceWrapper;
+import org.apache.submarine.server.submitter.yarnservice.YarnServiceJobSubmitter;
+import org.apache.submarine.server.submitter.yarnservice.tensorflow.component.TensorBoardComponent;
+import org.apache.submarine.server.submitter.yarnservice.utils.ZipUtilities;
 import org.apache.hadoop.yarn.util.resource.Resources;
 import org.junit.After;
 import org.junit.Assert;
--- submarine-server/server-submitter/submitter-yarnservice/src/test/java/org/apache/submarine/client/cli/yarnservice/TestYarnServiceRunJobCliCommons.java
+++ submarine-server/server-submitter/submitter-yarnservice/src/test/java/org/apache/submarine/client/cli/yarnservice/TestYarnServiceRunJobCliCommons.java
@@ -6,12 +6,15 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- *     http://www.apache.org/licenses/LICENSE-2.0
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
  */
 
 package org.apache.submarine.client.cli.yarnservice;
@@ -20,10 +23,10 @@ import org.apache.hadoop.yarn.client.api.AppAdminClient;
 import org.apache.hadoop.yarn.exceptions.YarnException;
 import org.apache.hadoop.yarn.service.api.records.Service;
 import org.apache.submarine.FileUtilitiesForTests;
-import org.apache.submarine.common.conf.SubmarineLogs;
-import org.apache.submarine.runtimes.common.JobSubmitter;
-import org.apache.submarine.runtimes.yarnservice.YarnServiceJobSubmitter;
-import org.apache.submarine.runtimes.yarnservice.YarnServiceUtils;
+import org.apache.submarine.commons.runtime.conf.SubmarineLogs;
+import org.apache.submarine.commons.runtime.JobSubmitter;
+import org.apache.submarine.server.submitter.yarnservice.YarnServiceJobSubmitter;
+import org.apache.submarine.server.submitter.yarnservice.YarnServiceUtils;
 
 import java.io.IOException;
 
