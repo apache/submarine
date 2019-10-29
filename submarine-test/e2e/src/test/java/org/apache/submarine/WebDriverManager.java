f51a30a43c7a6cc23cc3fccf6893c34e2f75fc77
kevin85421 <b03901153@ntu.edu.tw>
2019-10-29 14:42:37 +0800

    SUBMARINE-180. E2E Test Framework
    ### What is this PR for?
    To promise the functionality of workbench-web, we add the E2E test framework to this project.
    
    ### What type of PR is it?
    Feature
    
    ### Todos
    * Check the accuracy of E2E test framework on different OS.
    * After the web page finishes, we will add more front-end test cases.
    
    ### What is the Jira issue?
    * https://issues.apache.org/jira/browse/SUBMARINE-180
    
    ### How should this be tested?
    
    * [CI Pass](https://travis-ci.org/apache/submarine/builds/604295591?utm_source=github_status&utm_medium=notification)
    
    * Step1:
    ```
    cd hadoop-submarine/submarine-test/e2e
    ```
    * Step2-1: (Ubuntu)
    ```
    mvn verify
    ```
    * Step2-2: (MacOS)
    ```
    mvn clean install -U
    ```
    * After we execute the command in Step2, it will open the firefox browser, and then enter the string "admin" in the "id" and "password" blank.
    * The result is showed in the following screenshot.
    ### Screenshots (if appropriate)
    ![螢幕快照 2019-10-29 上午12 57 48](https://user-images.githubusercontent.com/20109646/67699523-3d235c80-f9e7-11e9-9789-c6249e25159f.png)
    
    ### Questions:
    * Does the licenses files need update? No
    * Is there breaking changes for older versions? No
    * Does this needs documentation? Yes
    
    Author: kevin85421 <b03901153@ntu.edu.tw>
    Author: xunix huang <qiwei_huang@qq.com>
    
    Closes #69 from kevin85421/e2e and squashes the following commits:
    
    b61b9b1 [kevin85421] SUBMARINE-180 [Umbrella] Delete submarine-workbench/workbench-web/tests/e2e-test
    a15dbf4 [kevin85421] SUBMARINE-180 [Umbrella] Update LOG & coding style
    dee105c [kevin85421] [SUBMARINE-180] E2E test framework
    9d5824c [xunix huang] SUBMARINE-180 [Umbrella] Submarine E2E Test Framework

---
+++ submarine-test/e2e/src/test/java/org/apache/submarine/WebDriverManager.java
@@ -0,0 +1,120 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.submarine;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang3.SystemUtils;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.rauschig.jarchivelib.Archiver;
+import org.rauschig.jarchivelib.ArchiverFactory;
+import java.io.File;
+import java.net.URL;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class WebDriverManager {
+    private static Logger LOG = LoggerFactory.getLogger(WebDriverManager.class);
+    private static String GECKODRIVER_VERSION = "0.25.0";
+    private static String downLoadsDir = "";
+    private static CommandExecutor cmdExec = new CommandExecutor();
+
+    public static WebDriver getWebDriver() {
+        WebDriver driver = null;
+        // firefox webdriver
+        if(driver == null) { 
+            try {
+                // download GeckoDriver
+                downLoadsDir = System.getProperty("user.dir");
+                String tempPath = downLoadsDir + "/Driver/";
+                downloadGeckoDriver(tempPath);
+                if(SystemUtils.IS_OS_MAC_OSX) {
+                    String command = "chmod +x " + tempPath + "geckodriver"; 
+                    cmdExec.executeCommandLocalHost(command);
+                }
+                System.setProperty("webdriver.gecko.driver", tempPath + "geckodriver");
+                // TODO(Kai-Hsun Chen): set firefox preference (refer to WebDriverManager.java:74 ~ 94 in Zeppelin)
+                // Initialize firefox WebDriver
+                String firefoxVersion = getFirefoxVersion();
+                LOG.info("Firefox version " + firefoxVersion + " detected");
+                driver = new FirefoxDriver();
+            } catch (Exception e) {
+                LOG.info("Exception in WebDriverManager while FireFox Driver");
+            }
+        }
+        return driver;
+    }
+    // TODO(Kai-Hsun Chen): need to set the path of geckodriver 
+    public static void downloadGeckoDriver(String tempPath) {
+        String geckoDriverUrlString = "https://github.com/mozilla/geckodriver/releases/download/v" + GECKODRIVER_VERSION + "/geckodriver-v" + GECKODRIVER_VERSION + "-";
+        LOG.info("Gecko version: v" + GECKODRIVER_VERSION + ", will be downloaded to " + tempPath);
+        try {
+            if (SystemUtils.IS_OS_WINDOWS) {
+                if (System.getProperty("sun.arch.data.model").equals("64")) {
+                  geckoDriverUrlString += "win64.zip";
+                } else {
+                  geckoDriverUrlString += "win32.zip";
+                }
+            } else if (SystemUtils.IS_OS_LINUX) {
+                if (System.getProperty("sun.arch.data.model").equals("64")) {
+                  geckoDriverUrlString += "linux64.tar.gz";
+                } else {
+                  geckoDriverUrlString += "linux32.tar.gz";
+                }
+            } else if (SystemUtils.IS_OS_MAC_OSX) {
+                geckoDriverUrlString += "macos.tar.gz";
+            }
+            
+            File geckoDriver = new File(tempPath + "geckodriver");
+            File geckoDriverZip = new File(tempPath + "geckodriver.tar");
+            File geckoDriverDir = new File(tempPath);
+            URL geckoDriverUrl = new URL(geckoDriverUrlString);
+            if (!geckoDriver.exists()) {
+              FileUtils.copyURLToFile(geckoDriverUrl, geckoDriverZip);
+              if (SystemUtils.IS_OS_WINDOWS) {
+                Archiver archiver = ArchiverFactory.createArchiver("zip");
+                archiver.extract(geckoDriverZip, geckoDriverDir);
+              } else {
+                Archiver archiver = ArchiverFactory.createArchiver("tar", "gz");
+                archiver.extract(geckoDriverZip, geckoDriverDir);
+              }
+            } else {
+                LOG.info("Gecko version: v" + GECKODRIVER_VERSION + " has already existed in path " + tempPath);
+                return;
+            }
+        } catch (Exception e) {
+            LOG.info("[FAIL] Download of Gecko version: v" + GECKODRIVER_VERSION + ", falied in path " + tempPath);
+            return;
+        }
+        LOG.info("[SUCCESS] Download of Gecko version: " + GECKODRIVER_VERSION); 
+    }
+    // TODO(Kai-Hsun Chen): need to be tested on Windows, MacOS, and Linux
+    public static String getFirefoxVersion() {
+        String firefoxVersionCmd = "firefox -v";
+        String version = "";
+        if (System.getProperty("os.name").startsWith("Mac OS")) {
+            firefoxVersionCmd = "/Applications/Firefox.app/Contents/MacOS/" + firefoxVersionCmd;
+        }
+        try {
+            version = cmdExec.executeCommandLocalHost(firefoxVersionCmd).toString(); 
+        } catch (Exception e) {
+            LOG.info("Exception in WebDriverManager while getFirefoxVersion");
+        }
+        return version;            
+    }
+}
