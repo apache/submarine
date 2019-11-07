ae3043f59a2753da4bce9c88ec74187409eab408
jasoonn <b05901171@ntu.edu.tw>
2019-11-07 14:33:10 +0800

    update setting prot

--- submarine-test/e2e/src/test/java/org/apache/submarine/WebDriverManager.java
+++ submarine-test/e2e/src/test/java/org/apache/submarine/WebDriverManager.java
@@ -57,6 +57,8 @@ public class WebDriverManager {
                 LOG.info("Exception in WebDriverManager while FireFox Driver");
             }
         }
+        String url = "http://127.0.0.1:8000";
+        driver.get(url);
         return driver;
     }
     // TODO(Kai-Hsun Chen): need to set the path of geckodriver 
