0b26558e88e9730f725e278d11b86bbd5ca11856
Xun Liu <liuxun@apache.org>
2019-12-09 14:34:07 +0800

    SUBMARINE-314. Creating a k8s environment locally using kind
    ### What is this PR for?
    Because the submarine on cloud is based on the k8s operating environment, in order to be able to develop and test locally, we use kind to deploy a complete k8s environment locally.
    1. install k8s cluster
    2. install docker registry
    
    ### What type of PR is it?
    Feature
    
    ### Todos
    * [ ] - Task
    
    ### What is the Jira issue?
    * https://issues.apache.org/jira/browse/SUBMARINE-314
    
    ### How should this be tested?
    * [CI Pass](https://travis-ci.org/liuxunorg/submarine/builds/622469321)
    
    ### Screenshots (if appropriate)
    ![image](https://user-images.githubusercontent.com/3677382/70410935-accb4380-1a8b-11ea-9549-532a6ae0429a.png)
    
    ```
    kubectl get pods -A
    NAMESPACE     NAME                       READY   STATUS    RESTARTS   AGE
    kube-system   coredns-576cbf47c7-mk5bb   0/1     Pending   0          27s
    kube-system   coredns-576cbf47c7-zbhgz   0/1     Pending   0          27s
    kube-system   ip-masq-agent-fdzr5        1/1     Running   0          27s
    kube-system   ip-masq-agent-w2pgc        1/1     Running   0          25s
    kube-system   kindnet-hbclv              1/1     Running   0          25s
    kube-system   kindnet-vlx47              1/1     Running   0          27s
    kube-system   kube-proxy-84c9z           1/1     Running   0          25s
    kube-system   kube-proxy-mvvbz           1/1     Running   0          27s
    ```
    
    ```
    docker tag busybox localhost:5000/busybox
    docker push localhost:5000/busybox
    The push refers to repository [localhost:5000/busybox]
    1da8e4c8d307: Pushed
    latest: digest: sha256:679b1c1058c1f2dc59a3ee70eed986a88811c0205c8ceea57cec5f22d2c3fbb1 size: 527
    ```
    
    ### Questions:
    * Does the licenses files need update? Yes/No
    * Is there breaking changes for older versions? Yes/No
    * Does this needs documentation? Yes/No
    
    Author: Xun Liu <liuxun@apache.org>
    
    Closes #123 from liuxunorg/SUBMARINE-314 and squashes the following commits:
    
    018aa2f [Xun Liu] Add auto download kubectl & kind desc.
    8245746 [Xun Liu] SUBMARINE-314. Creating a k8s environment locally using kind

---
+++ submarine-cloud/hack/lib.sh
@@ -0,0 +1,78 @@
+#!/usr/bin/env bash
+#
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+if [ -z "$ROOT" ]; then
+    echo "error: ROOT should be initialized"
+    exit 1
+fi
+
+OS=$(go env GOOS)
+ARCH=$(go env GOARCH)
+OUTPUT=${ROOT}/output
+OUTPUT_BIN=${OUTPUT}/bin
+KUBECTL_VERSION=1.12.10
+KUBECTL_BIN=$OUTPUT_BIN/kubectl
+KIND_VERSION=0.6.0
+KIND_BIN=$OUTPUT_BIN/kind
+
+test -d "$OUTPUT_BIN" || mkdir -p "$OUTPUT_BIN"
+
+function hack::verify_kubectl() {
+    if test -x "$KUBECTL_BIN"; then
+        [[ "$($KUBECTL_BIN version --client --short | grep -o -E '[0-9]+\.[0-9]+\.[0-9]+')" == "$KUBECTL_VERSION" ]]
+        return
+    fi
+    return 1
+}
+
+function hack::ensure_kubectl() {
+    if hack::verify_kubectl; then
+        return 0
+    fi
+    echo "Installing kubectl v$KUBECTL_VERSION..."
+    tmpfile=$(mktemp)
+    trap "test -f $tmpfile && rm $tmpfile" RETURN
+    curl --retry 10 -L -o $tmpfile https://storage.googleapis.com/kubernetes-release/release/v${KUBECTL_VERSION}/bin/${OS}/${ARCH}/kubectl
+    mv $tmpfile $KUBECTL_BIN
+    chmod +x $KUBECTL_BIN
+}
+
+function hack::verify_kind() {
+    if test -x "$KIND_BIN"; then
+        [[ "$($KIND_BIN --version 2>&1 | cut -d ' ' -f 3)" == "$KIND_VERSION" ]]
+        return
+    fi
+    return 1
+}
+
+function hack::ensure_kind() {
+    if hack::verify_kind; then
+        return 0
+    fi
+    echo "Installing kind v$KIND_VERSION..."
+    tmpfile=$(mktemp)
+    trap "test -f $tmpfile && rm $tmpfile" RETURN
+    curl --retry 10 -L -o $tmpfile https://github.com/kubernetes-sigs/kind/releases/download/v${KIND_VERSION}/kind-$(uname)-amd64
+    mv $tmpfile $KIND_BIN
+    chmod +x $KIND_BIN
+}
+
+# hack::version_ge "$v1" "$v2" checks whether "v1" is greater or equal to "v2"
+function hack::version_ge() {
+    [ "$(printf '%s\n' "$1" "$2" | sort -V | head -n1)" = "$2" ]
+}
