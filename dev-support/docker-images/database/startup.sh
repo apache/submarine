61340cda4b05071a44896116f08ee7cf0938b8df
Xun Liu <liuxun@apache.org>
2020-01-12 11:07:21 +0800

    SUBMARINE-340. Add submarine database dockerfile
    ### What is this PR for?
    In order to enable submarine to support deployment in k8s,
    We need to enable each service of submarine to be deployed independently.
    The problem that jira solves is the ability to install the submarine table structure and initialization data into mysql.
    Allows users to run submarine's database service with only one command.
    like.,
    ```
    docker run -it -p 3306:3306 -d --name s1 -e MYSQL_ROOT_PASSWORD=password apache/submarine:database-0.3.0-SNAPSHOT
    ```
    
    ### What type of PR is it?
    [Refactoring]
    
    ### Todos
    * [ ] - Task
    
    ### What is the Jira issue?
    * https://issues.apache.org/jira/browse/SUBMARINE-340
    
    ### How should this be tested?
    * https://travis-ci.org/liuxunorg/submarine/builds/635299028
    
    ### Screenshots (if appropriate)
    
    ### Questions:
    * Does the licenses files need update? No
    * Is there breaking changes for older versions? No
    * Does this needs documentation? Yes
    
    Author: Xun Liu <liuxun@apache.org>
    
    Closes #147 from liuxunorg/SUBMARINE-340 and squashes the following commits:
    
    fd1c1a5 [Xun Liu] Fixed version error.
    b39a156 [Xun Liu] SUBMARINE-340. Add submarine database dockerfile

---
+++ dev-support/docker-images/database/startup.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+mysql -uroot -p$MYSQL_ROOT_PASSWORD <<EOF
+CREATE DATABASE submarine_test;
+CREATE USER 'submarine_test'@'%' IDENTIFIED BY 'password_test';
+GRANT ALL PRIVILEGES ON *.* TO 'submarine_test'@'%';
+use submarine_test; source /tmp/database/submarine.sql; source /tmp/database/submarine-data.sql;
+CREATE DATABASE submarine;
+CREATE USER 'submarine'@'%' IDENTIFIED BY 'password';
+GRANT ALL PRIVILEGES ON *.* TO 'submarine'@'%';
+use submarine; source /tmp/database/submarine.sql; source /tmp/database/submarine-data.sql;
+CREATE DATABASE metastore_test;
+CREATE USER 'metastore_test'@'%' IDENTIFIED BY 'password_test';
+GRANT ALL PRIVILEGES ON * . * TO 'metastore_test'@'%';
+use metastore_test; source /tmp/database/metastore.sql;
+CREATE DATABASE metastore;
+CREATE USER 'metastore'@'%' IDENTIFIED BY 'password';
+GRANT ALL PRIVILEGES ON * . * TO 'metastore'@'%';
+use metastore; source /tmp/database/metastore.sql;
+EOF
