ace5350882146215f12d0026313107d5356e2b3e
Kent Yao <yaooqinn@hotmail.com>
2020-03-12 11:30:57 +0800

    SUBMARINE-425. Optimize java style check in github actions
    ### What is this PR for?
    
    the current java style check does not show the root cause for us.
    
    ### What type of PR is it?
    Improvement
    ### Todos
    
    ### What is the Jira issue?
    * Open an issue on Jira https://issues.apache.org/jira/browse/SUBMARINE-425
    * Put link here, and add [SUBMARINE-*Jira number*] in PR title, eg. [SUBMARINE-23]
    
    ### How should this be tested?
    pass GitHub action
    
    ### Screenshots (if appropriate)
    
    ### Questions:
    * Does the licenses files need update? No
    * Is there breaking changes for older versions? No
    * Does this needs documentation? No
    
    Author: Kent Yao <yaooqinn@hotmail.com>
    
    Closes #224 from yaooqinn/SUBMARINE-425 and squashes the following commits:
    
    4040cd1 [Kent Yao] SUBMARINE-425. Optimize java style check in github actions

---
+++ dev-support/lint-java.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+#
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+export MAVEN_OPTS="-Xmx2g -XX:ReservedCodeCacheSize=512m  -Dorg.slf4j.simpleLogger.defaultLogLevel=WARN"
+mvn --no-transfer-progress install -DskipTests -pl "!org.apache.submarine:submarine-cloud"
+
+ERRORS=$(mvn checkstyle:check | grep ERROR)
+
+if test ! -z "$ERRORS"; then
+    echo -e "Checkstyle checks failed at following occurrences:\n$ERRORS"
+    find . -name checkstyle-output.xml | xargs cat | grep -v checkstyle | grep -v "<?xml"
+    exit 1
+else
+    echo -e "Checkstyle checks passed."
+fi
