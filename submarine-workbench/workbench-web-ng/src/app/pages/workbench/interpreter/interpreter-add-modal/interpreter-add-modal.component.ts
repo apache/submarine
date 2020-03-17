7e2ba55bff32830a1d5671e47238824b87003f7e
kevin85421 <b03901153@ntu.edu.tw>
2020-03-17 13:51:42 +0800

    SUBMARINE-430. [WEB] Implement frontend of Interpreter page in workbench with Angular

---
+++ submarine-workbench/workbench-web-ng/src/app/pages/workbench/interpreter/interpreter-add-modal/interpreter-add-modal.component.ts
@@ -0,0 +1,67 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import { Component, EventEmitter, Input, OnChanges, Output, SimpleChanges } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'submarine-interpreter-add-modal',
+  templateUrl: './interpreter-add-modal.component.html',
+  styleUrls: ['./interpreter-add-modal.component.scss']
+})
+export class InterpreterAddModalComponent implements OnChanges {
+  @Input() modalTitle: string;
+  @Input() interpreterName: string;
+  @Input() interpreterType: string;
+  @Input() visible: boolean;
+  @Output() readonly close: EventEmitter<any> = new EventEmitter();
+  @Output() readonly ok: EventEmitter<any> = new EventEmitter();
+  form: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      interpreterName: ['', Validators.required],
+      interpreterType: ['', Validators.required]
+    });
+  }
+
+  ngOnChanges (changes: SimpleChanges) {
+    this.form.reset({
+      interpreterName: this.interpreterName,
+      interpreterType: this.interpreterType
+    });
+  }
+
+  hideModal() {
+    this.close.emit();
+  }
+
+  submitForm() {
+    for (const key in this.form.controls) {
+      this.form.controls[key].markAsDirty();
+      this.form.controls[key].updateValueAndValidity();
+    }
+
+    if (!this.form.valid) {
+      return;
+    }
+
+    this.ok.emit(this.form.value);
+  }
+}
--- submarine-workbench/workbench-web-ng/src/app/pages/workbench/workbench.module.ts
+++ submarine-workbench/workbench-web-ng/src/app/pages/workbench/workbench.module.ts
@@ -19,31 +19,28 @@
 
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { WorkbenchRoutingModule } from '@submarine/pages/workbench/workbench-routing.module';
 import { NgZorroAntdModule } from 'ng-zorro-antd';
 import { DataComponent } from './data/data.component';
 import { HomeComponent } from './home/home.component';
-import { InterpreterComponent } from './interpreter/interpreter.component';
+import { InterpreterModule } from './interpreter/interpreter.module';
 import { JobComponent } from './job/job.component';
+import { JobModule } from './job/job.module';
 import { ModelComponent } from './model/model.component';
 import { WorkbenchComponent } from './workbench.component';
 import { WorkspaceComponent } from './workspace/workspace.component';
-import { FormsModule } from '@angular/forms';
 import { WorkspaceModule } from './workspace/workspace.module';
-import { jobModule } from './job/job.module';
-
-
 
 @NgModule({
   declarations: [
     WorkbenchComponent,
     HomeComponent,
     WorkspaceComponent,
-    InterpreterComponent,
     JobComponent,
     DataComponent,
-    ModelComponent,
+    ModelComponent
   ],
   imports: [
     CommonModule,
@@ -52,7 +49,8 @@ import { jobModule } from './job/job.module';
     RouterModule,
     FormsModule,
     WorkspaceModule,
-    jobModule
+    JobModule,
+    InterpreterModule
   ]
 })
 export class WorkbenchModule {
