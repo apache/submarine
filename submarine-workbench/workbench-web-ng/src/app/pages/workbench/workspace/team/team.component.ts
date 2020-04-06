7b2bcb79697e761e2fbcb07382d8236d345d38de
kobe860219 <kobe860219@gmail.com>
2020-04-06 15:25:45 +0800

    SUBMARINE-454. Implement frontend of team page in workbench with Angular
    ### What is this PR for?
    Implement frontend of team page in workbench with Angular
    
    ### What type of PR is it?
    Feature
    
    ### Todos
    
    ### What is the Jira issue?
    https://issues.apache.org/jira/browse/SUBMARINE-454
    
    ### How should this be tested?
    https://travis-ci.org/github/kobe860219/submarine
    
    ### Screenshots (if appropriate)
    <img width="1436" alt="teamDemo" src="https://user-images.githubusercontent.com/48027290/78390800-c59dcd00-7617-11ea-820e-c16f7c8d9b83.png">
    
    https://imgur.com/84kc6WV
    
    ### Questions:
    * Does the licenses files need update? No
    * Is there breaking changes for older versions? No
    * Does this needs documentation? No
    
    Author: kobe860219 <kobe860219@gmail.com>
    
    Closes #255 from kobe860219/SUBMARINE-454 and squashes the following commits:
    
    fea0709 [kobe860219] Add license in teamIT.java e2e test file.
    452a5e0 [kobe860219] Fix the problem in comment and add teamIT.java e2e test file.
    6240107 [kobe860219] SUBMARINE-454. Implement frontend of team page in workbench with Angular

--- submarine-workbench/workbench-web-ng/src/app/pages/workbench/workspace/team/team.component.ts
+++ submarine-workbench/workbench-web-ng/src/app/pages/workbench/workspace/team/team.component.ts
@@ -18,17 +18,166 @@
  */
 
 import { Component, OnInit } from '@angular/core';
+import { NzMessageService } from 'ng-zorro-antd/message';
+import { NzNotificationService } from 'ng-zorro-antd/notification';
 
 @Component({
   selector: 'app-team',
   templateUrl: './team.component.html',
   styleUrls: ['./team.component.scss']
 })
-export class TeamComponent implements OnInit {
-  isSpinning = true;
-  constructor() { }
+export class TeamComponent implements OnInit {  
+  
+  // Get into a Team or not
+  isEntering = false;
+
+  //Get Current Team Data (Now Use Simulated Data) 
+  currentTeamName: string;
+  currentTeamDepartment: string;
+  currentTeamDescription: string;
+  currentTeamProjectNum: Number;
+  currentTeamMemberNum: Number;
+  currentTeamSettingPermission: boolean;
+
+  // Is Editging Members or not
+  isEditingMember = false;
+
+  // For CreateTeamModal
+  createTeamModalIsVisible = false;
+
+  // For AddUserModal
+  addUserModalIsVisible = false;
+  addUserIsOkLoading = false;
+
+  // Simulated Data for Members
+  member = [
+    {
+      name: 'Anna',
+      email: 'test@mail.com',
+      permission: 'admin'
+    },
+    {
+      name: 'James',
+      email: 'test@mail.com',
+      permission: 'admin'
+    },
+    {
+      name: 'Jack',
+      email: 'test@mail.com',
+      permission: 'contributer'
+    },
+    {
+      name: 'Ken',
+      email: 'test@mail.com',
+      permission: 'viwer'
+    }
+  ]
+
+  // Simulated Data for Teams
+  existTeams = [
+    {
+      name: "Submarine",
+      department: "Apache",
+      description: "Something about this team...",
+      projectNum: 3,
+      memberNum: 3,
+      role: 'admin',
+      settingPermission: true
+    },
+    {
+      name: "Team2",
+      department: "Apple",
+      description: "Something about this team...",
+      projectNum: 3,
+      memberNum: 3,
+      role: 'viwer',
+      settingPermission: false
+    },
+  ]
+  constructor(
+    private nzMessageService: NzMessageService, 
+    private notification: NzNotificationService,
+    ) { }
 
   ngOnInit() {
   }
 
+  enter(team) {
+    this.isEntering = true;
+    this.currentTeamName = team.name;
+    this.currentTeamDepartment = team.department;
+    this.currentTeamDescription = team.description;
+    this.currentTeamProjectNum = team.projectNum;
+    this.currentTeamMemberNum = team.memberNum;
+    this.currentTeamSettingPermission = team.settingPermission;
+  }
+
+  startCreateTeam() {
+    this.createTeamModalIsVisible = true;
+  }
+
+  startEditMember() {
+    this.isEditingMember=true;
+  }
+
+  saveEditMember() {
+    this.isEditingMember=false;
+  }
+
+  cancel(): void {
+  }
+
+  confirm(): void {
+    this.nzMessageService.info('Delete Successful!');
+  }
+
+  // For CreateTeamModal
+  createTeamOk() {
+    this.createTeamModalIsVisible = false;
+    console.log("Create Seuccessful!");
+  }
+ 
+
+  // For AddUserModal
+  startAddUser(): void {
+    this.addUserModalIsVisible = true;
+  }
+
+  // Add Success
+  createNotification(type: string): void {
+    this.notification.create(
+      type,
+      'Add Successful!',
+      'Make sure that user check invitation!'
+    );
+  }
+  
+  // For AddUserModal
+  addUserOk(): void {
+    this.addUserIsOkLoading = true;
+    setTimeout(() => {
+      this.addUserModalIsVisible = false;
+      this.addUserIsOkLoading = false;
+      this.createNotification("success");
+    }, 1000);
+  }
+
+  // For AddUserModal
+  addUserCancel(): void {
+    this.addUserModalIsVisible = false;
+  }
+
+  //TODO(kobe860219) : Get Team From DataBase
+  getTeamDataFromDB() {
+
+  }
+
+  //TODO(kobe860219) : Get User From DataBase
+  getUserDataFromDB() {
+  }
+
+  //TODO(kobe860219) : Update Data to DataBase
+  updateTeamData() {
+
+  }
 }
