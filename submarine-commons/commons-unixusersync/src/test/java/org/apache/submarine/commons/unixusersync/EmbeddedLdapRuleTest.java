febe149fd797ea68878a1e8511627a4a487e5755
Eroschang <sanchang274@gmail.com>
2020-05-13 02:15:35 +0800

    SUBMARINE-490. Synchronize user information from LDAP
    ### What is this PR for?
    Test to synchronize user information from LDAP server.
    
    ### What type of PR is it?
    Feature
    
    ### Todos
    * [ ] - Task
    
    ### What is the Jira issue?
    * Open an issue on Jira https://issues.apache.org/jira/browse/SUBMARINE/
    * Put link here, and add [SUBMARINE-*Jira number*] in PR title, eg. [SUBMARINE-23]
    https://issues.apache.org/jira/projects/SUBMARINE/issues/SUBMARINE-490?filter=allopenissues&orderby=priority+ASC%2C+updated+DESC
    
    ### How should this be tested?
    * First time? Setup Travis CI as described on https://submarine.apache.org/contribution/contributions.html#continuous-integration
    * Strongly recommended: add automated unit tests for any new or changed behavior
    * Outline any manual steps to test the PR here.
    
    ### Screenshots (if appropriate)
    Edit the example.ldif file to add user in LDAP server.
    ![image](https://user-images.githubusercontent.com/43379142/81664370-fd4b3080-9471-11ea-9998-ab07b127831a.png)
    
    Use testList() to test and get the user name which in LDAP server.
    ![image](https://user-images.githubusercontent.com/43379142/81664430-0a681f80-9472-11ea-8412-03b92789516a.png)
    
    The user name in LDAP server.
    ![image](https://user-images.githubusercontent.com/43379142/81664518-1e138600-9472-11ea-88c5-e2d0ed03584a.png)
    
    ### Questions:
    * Does the licenses files need update? No
    * Is there breaking changes for older versions? No
    * Does this needs documentation? No
    
    Author: Eroschang <sanchang274@gmail.com>
    
    Closes #286 from Eroschang/submarine-ldaptest and squashes the following commits:
    
    5d59787 [Eroschang] Add Logger to output log.
    e5dac0c [Eroschang] Add embedded-ldap-junit.version number.
    340446a [Eroschang] Make the version number to be variable.
    d88b142 [Eroschang] Fix checkstyle.
    c6ff2ed [Eroschang] Fix checkstyle.
    2236cc7 [Eroschang] Change the package name and add apache license head.
    0860bd6 [Eroschang] Add apache license head.
    ccdd846 [Eroschang] Add LDAP user test.

---
+++ submarine-commons/commons-unixusersync/src/test/java/org/apache/submarine/commons/unixusersync/EmbeddedLdapRuleTest.java
@@ -0,0 +1,142 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.submarine.commons.unixusersync;
+
+import com.google.common.collect.Iterators;
+
+import com.unboundid.ldap.sdk.AddRequest;
+import com.unboundid.ldap.sdk.Attribute;
+import com.unboundid.ldap.sdk.LDAPConnection;
+import com.unboundid.ldap.sdk.LDAPInterface;
+import com.unboundid.ldap.sdk.SearchRequest;
+import com.unboundid.ldap.sdk.SearchResult;
+import com.unboundid.ldap.sdk.SearchResultEntry;
+import com.unboundid.ldap.sdk.SearchScope;
+import org.junit.Rule;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.zapodot.junit.ldap.EmbeddedLdapRule;
+import org.zapodot.junit.ldap.EmbeddedLdapRuleBuilder;
+
+import javax.naming.Context;
+import javax.naming.NameClassPair;
+import javax.naming.NamingEnumeration;
+import javax.naming.directory.DirContext;
+import javax.naming.directory.SearchControls;
+import java.util.Arrays;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+public class EmbeddedLdapRuleTest {
+
+  public static final String DOMAIN_DSN = "dc=zapodot,dc=org";
+  private static final Logger LOG = LoggerFactory.getLogger(EmbeddedLdapRuleTest.class);
+
+  @Rule
+  public EmbeddedLdapRule embeddedLdapRule = EmbeddedLdapRuleBuilder
+          .newInstance()
+          .usingDomainDsn(DOMAIN_DSN)
+          .importingLdifs("example.ldif")
+          .build();
+
+  @Test
+  public void testLdapConnection() throws Exception {
+    final LDAPInterface ldapConnection = embeddedLdapRule.ldapConnection();
+    final SearchResult searchResult =
+            ldapConnection.search(DOMAIN_DSN, SearchScope.SUB, "(objectClass=person)");
+
+    assertEquals(2, searchResult.getEntryCount());
+  }
+
+  @Test
+  public void testRawLdapConnection() throws Exception {
+    final String commonName = "Test person";
+    final String dn = String.format("cn=%s,ou=people,dc=zapodot,dc=org", commonName);
+    LDAPConnection ldapConnection = embeddedLdapRule.unsharedLdapConnection();
+    try {
+      ldapConnection.add(new AddRequest(dn, Arrays.asList(
+              new Attribute("objectclass", "top",
+                      "person", "organizationalPerson", "inetOrgPerson"),
+              new Attribute("cn", commonName), new Attribute("sn", "Person"),
+              new Attribute("uid", "test"))));
+    } finally {
+      // Forces the LDAP connection to be closed.
+      // This is not necessary as the rule will usually close it for you.
+      ldapConnection.close();
+    }
+    ldapConnection = embeddedLdapRule.unsharedLdapConnection();
+    final SearchResultEntry entry = ldapConnection.searchForEntry(new SearchRequest(dn,
+          SearchScope.BASE,
+          "(objectClass=person)"));
+    assertNotNull(entry);
+  }
+
+  @Test
+  public void testDirContext() throws Exception {
+    final DirContext dirContext = embeddedLdapRule.dirContext();
+    final SearchControls searchControls = new SearchControls();
+    searchControls.setSearchScope(SearchControls.SUBTREE_SCOPE);
+    final NamingEnumeration<javax.naming.directory.SearchResult> resultNamingEnumeration =
+            dirContext.search(DOMAIN_DSN, "(objectClass=person)", searchControls);
+    assertEquals(2, Iterators.size(Iterators.forEnumeration(resultNamingEnumeration)));
+  }
+
+  @Test
+  public void testContext() throws Exception {
+    final Context context = embeddedLdapRule.context();
+    final Object user = context.lookup("cn=Eros,ou=people,dc=zapodot,dc=org");
+    assertNotNull(user);
+  }
+
+  @Test
+  public void testList() throws Exception {
+    final Context context = embeddedLdapRule.context();
+    NamingEnumeration list = context.list("ou=semi-people,dc=zapodot,dc=org");
+
+    while (list.hasMore()){
+      NameClassPair nc = (NameClassPair) list.next();
+      System.out.println(nc.getName());
+    }
+
+    context.close();
+  }
+
+  @Test
+  public void testContextClose() throws Exception {
+    final Context context = embeddedLdapRule.context();
+    context.close();
+    assertNotNull(context.getNameInNamespace());
+  }
+
+  @Test
+  public void testEmbeddedServerPort() throws Exception {
+    assertTrue(embeddedLdapRule.embeddedServerPort() > 0);
+  }
+
+  private void assertTrue(boolean b) {
+  }
+
+  @Test(expected = IllegalStateException.class)
+  public void testNoPortAssignedYet() throws Exception {
+    final EmbeddedLdapRule embeddedLdapRule = new EmbeddedLdapRuleBuilder().build();
+    embeddedLdapRule.embeddedServerPort();
+  }
+}
