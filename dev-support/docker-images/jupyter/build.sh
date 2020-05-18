9c2cdf83beaec7f3ad24066b3a83121ef0e0b370
Ryan Lo <lowc1012@gmail.com>
2020-05-18 09:52:35 +0800

    SUBMARINE-493. Integrating jupyter notebook into Submarine
    ### What is this PR for?
    Deploy jupyter notebook with submarine-server
    
    ### What type of PR is it?
    [Feature]
    
    ### Todos
    
    ### What is the Jira issue?
    [SUBMARINE-493](https://issues.apache.org/jira/browse/SUBMARINE-493)
    
    ### How should this be tested?
    [passed CI](https://travis-ci.org/github/lowc1012/submarine/builds/688019593)
    
    ### Screenshots (if appropriate)
    <img width="1273" alt="screenshot1" src="https://user-images.githubusercontent.com/52355146/82148056-a20baa80-9884-11ea-8c77-30fe8ed741dd.png">
    
    ![screenshot2](https://user-images.githubusercontent.com/52355146/82148067-b64fa780-9884-11ea-9b8b-c434bd63bb01.png)
    
    ### Questions:
    * Does the licenses files need update? No
    * Is there breaking changes for older versions? No
    * Does this needs documentation? No
    
    Author: Ryan Lo <lowc1012@gmail.com>
    
    Closes #290 from lowc1012/SUBMARINE-493 and squashes the following commits:
    
    fdcba8d [Ryan Lo] SUBMARINE-493. Change the path of jupyter docker-images
    2c027d4 [Ryan Lo] SUBMARINE-493. typo fixed
    71dce2c [Ryan Lo] SUBMARINE-493. Integrating jupyter notebook into Submarine

---
+++ dev-support/docker-images/jupyter/build.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -eo pipefail
+set -x
+
+TF_JUPYTER_IMAGE="apache/submarine:tf2.1.0-jupyter"
+
+if [ -L ${BASH_SOURCE-$0} ]; then
+  PWD=$(dirname $(readlink "${BASH_SOURCE-$0}"))
+else
+  PWD=$(dirname ${BASH_SOURCE-$0})
+fi
+export CURRENT_PATH=$(cd "${PWD}">/dev/null; pwd)
+SUBMARINE_HOME=${CURRENT_PATH}/../../..
+
+# build image
+echo "Start building the ${TF_JUPYTER_IMAGE} docker image ..."
+cd ${CURRENT_PATH}
+docker build -t ${TF_JUPYTER_IMAGE} .
