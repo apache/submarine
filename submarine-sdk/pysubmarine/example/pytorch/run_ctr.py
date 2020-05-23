bdf1cbd588859a9060dd62f37b4f3fda6d30ae89
Andrew Hsieh <andrewhsiehth@gmail.com>
2020-05-23 17:38:31 +0800

    Add License

--- submarine-sdk/pysubmarine/example/pytorch/run_ctr.py
+++ submarine-sdk/pysubmarine/example/pytorch/run_ctr.py
@@ -13,13 +13,29 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from sklearn import metrics
-from types import FunctionType
 
 
-def get_metric_fn(key):
-    def _condition_fn(x):
-        k, v = x
-        return isinstance(v, FunctionType) and (k.lower() == key.lower())
-    _, metric_fn = next(iter(filter(_condition_fn, vars(metrics).items())))
-    return metric_fn
+from submarine.ml.pytorch.model.ctr import DeepFM
+
+import argparse
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-conf", help="a JSON configuration file for FM", type=str)
+    parser.add_argument("-task_type", default='train',
+                        help="train or evaluate, by default is train")
+    args = parser.parse_args()
+
+    trainer = DeepFM(json_path=args.conf)
+
+    if args.task_type == 'train':
+        trainer.fit()
+        print('[Train Done]')
+    elif args.task_type == 'evaluate':
+        score = trainer.evaluate()
+        print(f'Eval score: {score}')
+    elif args.task_type == 'predict':
+        pred = trainer.predict()
+        print('Predict:', pred)
+    else:
+        assert False, args.task_type
