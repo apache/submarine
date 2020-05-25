db848aa4c26b79881c4d60ecff4fbd0d88761f4d
Manikandan R <mani@caastle.com>
2020-05-25 11:45:49 +0530

    SUBMARINE-507: WIP POC patch. Patch has laid out the skeleton to ensure changes are on the right direction

---
+++ submarine-server/server-api/src/main/java/org/apache/submarine/server/api/spec/EnvironmentSpec.java
@@ -0,0 +1,100 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.submarine.server.api.spec;
+
+import java.util.Map;
+
+/**
+ * Submarine environment spec.
+ */
+public class EnvironmentSpec {
+
+  
+  /**
+   * Name of the environment
+   */
+  private String name;
+
+  /**
+   * VM-Image of the environment
+   */
+  private String vmImage;
+  
+  /**
+   * Docker-Image of the environment
+   */
+  private String dockerImage;
+  
+  /**
+   * Kernel Spec
+   */
+  private KernelSpec kernelSpec;
+  
+  /**
+   * Description
+   */
+  private String description;
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getVmImage() {
+    return vmImage;
+  }
+
+  public void setVmImage(String vmImage) {
+    this.vmImage = vmImage;
+  }
+
+  public String getDockerImage() {
+    return dockerImage;
+  }
+
+  public void setDockerImage(String dockerImage) {
+    this.dockerImage = dockerImage;
+  }
+
+  public KernelSpec getKernelSpec() {
+    return kernelSpec;
+  }
+
+  public void setKernelSpec(KernelSpec kernelSpec) {
+    this.kernelSpec = kernelSpec;
+  }
+
+  /**
+   * @return the description
+   */
+  public String getDescription() {
+    return description;
+  }
+
+  /**
+   * @param description the description to set
+   */
+  public void setDescription(String description) {
+    this.description = description;
+  }
+}
