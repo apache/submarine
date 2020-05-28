544cdb811aed382b4ddeb7c0272e61e23becd0ee
andrewhsiehth <39716613+andrewhsiehth@users.noreply.github.com>
2020-05-28 19:19:38 +0800

    Update submarine-sdk/pysubmarine/example/pytorch/run_ctr.py
    Co-authored-by: HUAN-PING SU <pingsutw@gmail.com>

--- submarine-sdk/pysubmarine/example/pytorch/run_ctr.py
+++ submarine-sdk/pysubmarine/example/pytorch/run_ctr.py
@@ -21,7 +21,7 @@ import argparse
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "-conf", help="a JSON configuration file for FM", type=str)
+        "-conf", help="a JSON configuration file for DeepFM", type=str)
     parser.add_argument("-task_type", default='train',
                         help="train or evaluate, by default is train")
     args = parser.parse_args()
