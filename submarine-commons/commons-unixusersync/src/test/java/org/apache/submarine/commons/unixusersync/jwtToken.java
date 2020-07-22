35cb72988e01ec77c3a8818f864e63fe336652f9
Eroschang <sanchang274@gmail.com>
2020-07-22 20:45:24 +0800

    rename this class.

--- submarine-commons/commons-unixusersync/src/test/java/org/apache/submarine/commons/unixusersync/jwtToken.java
+++ submarine-commons/commons-unixusersync/src/test/java/org/apache/submarine/commons/unixusersync/jwtToken.java
@@ -1,16 +1,33 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package org.apache.submarine.commons.unixusersync;
-
-
-import javax.crypto.spec.SecretKeySpec;
-import javax.xml.bind.DatatypeConverter;
-import java.security.Key;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.JwtBuilder;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.impl.crypto.MacProvider;
+import jdk.internal.jline.internal.Log;
 
+import javax.crypto.spec.SecretKeySpec;
+import javax.xml.bind.DatatypeConverter;
+import java.security.Key;
 import java.util.Base64;
 import java.util.Date;
 
@@ -23,42 +40,42 @@ public class jwtToken {
   //Sample method to construct a JWT
 
   public static String createJWT(String id, String issuer, String subject, long ttlMillis) {
-    //The JWT signature algorithm we will be using to sign the token
+  //The JWT signature algorithm we will be using to sign the token
     SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.HS256;
 
     long nowMillis = System.currentTimeMillis();
     Date now = new Date(nowMillis);
 
-    //We will sign our JWT with our ApiKey secret
+  //We will sign our JWT with our ApiKey secret
     Key signingKey = new SecretKeySpec(secretBytes, signatureAlgorithm.getJcaName());
 
-    //Let's set the JWT Claims
+  //Let's set the JWT Claims
     JwtBuilder builder = Jwts.builder().setId(id)
             .setIssuedAt(now)
             .setSubject(subject)
             .setIssuer(issuer)
             .signWith(signatureAlgorithm, signingKey);
 
-    //if it has been specified, let's add the expiration
+  //if it has been specified, let's add the expiration
     if (ttlMillis >= 0) {
       long expMillis = nowMillis + ttlMillis;
       Date exp = new Date(expMillis);
       builder.setExpiration(exp);
     }
 
-    //Builds the JWT and serializes it to a compact, URL-safe string
+  //Builds the JWT and serializes it to a compact, URL-safe string
     return builder.compact();
   }
 
   //Sample method to validate and read the JWT
   public void parseJWT(String jwt) {
-    //This line will throw an exception if it is not a signed JWS (as expected)
+  //This line will throw an exception if it is not a signed JWS (as expected)
     Claims claims = Jwts.parser()
         .setSigningKey(DatatypeConverter.parseBase64Binary(base64SecretBytes))
         .parseClaimsJws(jwt).getBody();
-    System.out.println("ID: " + claims.getId());
-    System.out.println("Subject: " + claims.getSubject());
-    System.out.println("Issuer: " + claims.getIssuer());
-    System.out.println("Expiration: " + claims.getExpiration());
+    Log.info("ID: " + claims.getId());
+    Log.info("Subject: " + claims.getSubject());
+    Log.info("Issuer: " + claims.getIssuer());
+    Log.info("Expiration: " + claims.getExpiration());
   }
 }
