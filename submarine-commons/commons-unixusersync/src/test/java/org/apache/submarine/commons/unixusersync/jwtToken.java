774c29e54d7900ddcefb98be896ebcc19f37632c
Eroschang <sanchang274@gmail.com>
2020-07-31 11:22:31 +0800

    Fix the annotation style.

--- submarine-commons/commons-unixusersync/src/test/java/org/apache/submarine/commons/unixusersync/jwtToken.java
+++ submarine-commons/commons-unixusersync/src/test/java/org/apache/submarine/commons/unixusersync/jwtToken.java
@@ -39,39 +39,39 @@ public class jwtToken {
   private static final String base64SecretBytes = Base64.getEncoder().encodeToString(secretBytes);
   private static final Logger LOG = LoggerFactory.getLogger(jwtToken.class);
 
-  //Sample method to construct a JWT
+  // Sample method to construct a JWT
 
   public static String createJWT(String id, String issuer, String subject, long ttlMillis) {
-    //The JWT signature algorithm we will be using to sign the token
+    // The JWT signature algorithm we will be using to sign the token
     SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.HS256;
 
     long nowMillis = System.currentTimeMillis();
     Date now = new Date(nowMillis);
 
-    //We will sign our JWT with our ApiKey secret
+    // We will sign our JWT with our ApiKey secret
     Key signingKey = new SecretKeySpec(secretBytes, signatureAlgorithm.getJcaName());
 
-    //Let's set the JWT Claims
+    // Let's set the JWT Claims
     JwtBuilder builder = Jwts.builder().setId(id)
             .setIssuedAt(now)
             .setSubject(subject)
             .setIssuer(issuer)
             .signWith(signatureAlgorithm, signingKey);
 
-    //if it has been specified, let's add the expiration
+    // if it has been specified, let's add the expiration
     if (ttlMillis >= 0) {
       long expMillis = nowMillis + ttlMillis;
       Date exp = new Date(expMillis);
       builder.setExpiration(exp);
     }
 
-    //Builds the JWT and serializes it to a compact, URL-safe string
+    // Builds the JWT and serializes it to a compact, URL-safe string
     return builder.compact();
   }
 
-  //Sample method to validate and read the JWT
+  // Sample method to validate and read the JWT
   public void parseJWT(String jwt) {
-    //This line will throw an exception if it is not a signed JWS (as expected)
+    // This line will throw an exception if it is not a signed JWS (as expected)
     Claims claims = Jwts.parser()
         .setSigningKey(DatatypeConverter.parseBase64Binary(base64SecretBytes))
         .parseClaimsJws(jwt).getBody();
