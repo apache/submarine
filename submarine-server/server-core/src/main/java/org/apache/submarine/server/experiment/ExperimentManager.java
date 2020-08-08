8449fe518163b8d583f794519b3cdd5545831a2b
Kevin Su <pingsutw@gmail.com>
2020-08-08 20:16:42 +0800

    Remove unused import

--- submarine-server/server-core/src/main/java/org/apache/submarine/server/experiment/ExperimentManager.java
+++ submarine-server/server-core/src/main/java/org/apache/submarine/server/experiment/ExperimentManager.java
@@ -20,7 +20,6 @@
 package org.apache.submarine.server.experiment;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
