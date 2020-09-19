0de920898f36fb0466dc61c38cb4dff7837c675b
pingsutw <pingsutw@gmail.com>
2020-09-19 19:42:33 +0800

    Update test

--- submarine-test/test-e2e/src/test/java/org/apache/submarine/integration/pages/ExperimentPage.java
+++ submarine-test/test-e2e/src/test/java/org/apache/submarine/integration/pages/ExperimentPage.java
@@ -63,13 +63,13 @@ public class ExperimentPage {
   @FindBy(name = "description")
   private WebElement description;
 
-  @FindBy(name = "namespace")
+  @FindBy(id = "namespace")
   private WebElement namespace;
 
   @FindBy(name = "cmd")
   private WebElement cmd;
 
-  @FindBy(name = "image")
+  @FindBy(id = "image")
   private WebElement image;
 
   // Env form
@@ -149,14 +149,17 @@ public class ExperimentPage {
   }
 
   // Real actions
-  public void fillExperimentMeta(String name, String description, String cmd, String image,
+  public void fillExperimentMeta(String name, String description, String namespace, String cmd, String image,
               String envKey, String envValue) {
     this.experimentName.clear();
     this.experimentName.sendKeys(name);
     this.description.clear();
     this.description.sendKeys(description);
+    Select objSelect = new Select(this.namespace);
+    objSelect.selectByVisibleText(namespace);
     this.cmd.clear();
     this.cmd.sendKeys(cmd);
+    this.image.clear();
     this.image.sendKeys(image);
     this.envKey.sendKeys(envKey);
     this.envValue.sendKeys(envValue);
