c641467cdad3927a2c08c85c09de8608c43f661b
kevin85421 <b03901153@ntu.edu.tw>
2020-10-05 16:16:44 +0800

    SUBMARINE-653. Replace workbench-web-ng with workbench-web

--- submarine-workbench/workbench-web-ng/karma.conf.js
+++ submarine-workbench/workbench-web-ng/karma.conf.js
@@ -36,7 +36,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/workbench-web-ng'),
+      dir: require('path').join(__dirname, './coverage/workbench-web'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
