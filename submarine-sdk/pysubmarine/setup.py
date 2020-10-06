3881dd1b5775ab9418796c2bada914d30d030c38
Kevin Su <pingsutw@apache.org>
2020-10-06 09:37:32 +0800

    SUBMARINE-655. Add a deepfm example in Jupyter notebook
    ### What is this PR for?
    To enhance the user experience of a notebook, we could add a deepfm example under the working directory (default is "/home/jovyan/") in the jupyter notebook.
    
    [deepfm_example.ipynb](https://github.com/pingsutw/hadoop-submarine/blob/8632f4ecdb3c38609f559e5e3a3eafb46cda9bd5/submarine-sdk/pysubmarine/example/deepfm_example.ipynb)
    
    ### What type of PR is it?
    [Improvement]
    
    ### Todos
    * [ ] - Task
    
    ### What is the Jira issue?
    https://issues.apache.org/jira/browse/SUBMARINE-655
    
    ### How should this be tested?
    https://travis-ci.org/github/pingsutw/hadoop-submarine/builds/733169607
    
    ### Screenshots (if appropriate)
    ![Screenshot from 2020-10-06 10-16-17](https://user-images.githubusercontent.com/37936015/95151444-42735480-07bd-11eb-99a4-6aa2dcdee13e.png)
    
    ### Questions:
    * Does the licenses files need update? No
    * Is there breaking changes for older versions? No
    * Does this needs documentation? No
    
    Author: Kevin Su <pingsutw@apache.org>
    
    Closes #430 from pingsutw/SUBMARINE-655 and squashes the following commits:
    
    fac5d81 [Kevin Su] SUBMARINE-655. Add a deepfm example in Jupyter notebook

--- submarine-sdk/pysubmarine/setup.py
+++ submarine-sdk/pysubmarine/setup.py
@@ -28,7 +28,7 @@ setup(
     packages=find_packages(exclude=['tests', 'tests.*']),
     install_requires=[
         'six>=1.10.0',
-        'numpy',
+        'numpy==1.18.5',
         'pandas',
         'sqlalchemy',
         'sqlparse',
