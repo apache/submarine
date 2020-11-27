1a58d0a596cf73265faf577f2456e10431338642
JohnTing <jot.johnting@gmail.com>
2020-11-28 02:43:54 +0800

    SUBMARINE-623. Experiment templates cannot use pure digital parameters
    ### What is this PR for?
    
    Now experiment templates cannot use pure digital parameters,
     For example,
    
    if the following "replicas", using
    `"{{Master.replicas}}"`, the experiment will fail.
     If the "replicas" uses
    `{{Master.replicas}}`
    , it will not be parsed as json.
    "spec": {
        "Master": {
          "replicas": 1,
          "resources": "cpu=1,memory=1024M"
        },
        "Worker": {
          "replicas": 1,
          "resources": "cpu=1,memory=1024M"
        }
      }
     
    This PR adds another way to map parameters
    It will replace the parameter according to the json path.
    ```json
    {
      "name": "spec.Master.replicas",
      "value": 1,
      "required": true,
      "description": "Ps replicas."
    }
    ```
    
    ### What type of PR is it?
    Bug Fix
    
    ### Todos
    * [x] - Fix Bug
    * [x] - Update documentation
    
    ### What is the Jira issue?
    https://issues.apache.org/jira/browse/SUBMARINE-623
    
    ### How should this be tested?
    
    ### Screenshots (if appropriate)
    
    ### Questions:
    * Does the licenses files need update? Yes/No
    * Is there breaking changes for older versions? Yes/No
    * Does this needs documentation? Yes/No
    
    Author: JohnTing <jot.johnting@gmail.com>
    
    Closes #415 from JohnTing/SUBMARINE-623 and squashes the following commits:
    
    fccc1f1 [JohnTing] test1
    a499fd3 [JohnTing] change

--- submarine-server/server-core/src/main/java/org/apache/submarine/server/experimenttemplate/ExperimentTemplateManager.java
+++ submarine-server/server-core/src/main/java/org/apache/submarine/server/experimenttemplate/ExperimentTemplateManager.java
@@ -21,8 +21,10 @@ package org.apache.submarine.server.experimenttemplate;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 import java.util.concurrent.atomic.AtomicInteger;
@@ -35,19 +37,26 @@ import org.apache.ibatis.session.SqlSession;
 import org.apache.submarine.commons.utils.exception.SubmarineRuntimeException;
 import org.apache.submarine.server.SubmarineServer;
 import org.apache.submarine.server.api.experiment.Experiment;
+import org.apache.submarine.server.api.experiment.ExperimentId;
 import org.apache.submarine.server.api.experimenttemplate.ExperimentTemplate;
 import org.apache.submarine.server.api.experimenttemplate.ExperimentTemplateId;
 import org.apache.submarine.server.api.experimenttemplate.ExperimentTemplateSubmit;
+import org.apache.submarine.server.api.spec.ExperimentSpec;
+import org.apache.submarine.server.api.spec.ExperimentTaskSpec;
 import org.apache.submarine.server.api.spec.ExperimentTemplateParamSpec;
 import org.apache.submarine.server.api.spec.ExperimentTemplateSpec;
 import org.apache.submarine.server.database.utils.MyBatisUtil;
 import org.apache.submarine.server.experiment.ExperimentManager;
 import org.apache.submarine.server.experimenttemplate.database.entity.ExperimentTemplateEntity;
 import org.apache.submarine.server.experimenttemplate.database.mappers.ExperimentTemplateMapper;
-
+import org.apache.submarine.server.gson.ExperimentIdDeserializer;
+import org.apache.submarine.server.gson.ExperimentIdSerializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.esotericsoftware.minlog.Log;
+import com.github.wnameless.json.flattener.JsonFlattener;
+import com.github.wnameless.json.unflattener.JsonUnflattener;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
@@ -57,10 +66,15 @@ import com.google.gson.GsonBuilder;
 public class ExperimentTemplateManager {
 
   private static final Logger LOG = LoggerFactory.getLogger(ExperimentTemplateManager.class);
-
   private static volatile ExperimentTemplateManager manager;
-
   private final AtomicInteger experimentTemplateIdCounter = new AtomicInteger(0);
+
+  private static final GsonBuilder gsonBuilder = new GsonBuilder()
+      .registerTypeAdapter(ExperimentId.class, new ExperimentIdSerializer())
+      .registerTypeAdapter(ExperimentId.class, new ExperimentIdDeserializer());
+  private static Gson gson = gsonBuilder.setDateFormat("yyyy-MM-dd HH:mm:ss").create();
+
+
 
   /**
    * ExperimentTemplate Cache
@@ -122,13 +136,17 @@ public class ExperimentTemplateManager {
   }
 
   private ExperimentTemplate createOrUpdateExperimentTemplate(ExperimentTemplateSpec spec, String operation) {
+
+    spec = addResourcesParameter(spec);
+
     ExperimentTemplateEntity entity = new ExperimentTemplateEntity();
     String experimentTemplateId = generateExperimentTemplateId().toString();
     entity.setId(experimentTemplateId);
     entity.setExperimentTemplateName(spec.getName());
-    entity.setExperimentTemplateSpec(new GsonBuilder().disableHtmlEscaping().create().toJson(spec));
+    entity.setExperimentTemplateSpec(gsonBuilder.disableHtmlEscaping().create().toJson(spec));
 
-    parameterMapping(entity.getExperimentTemplateSpec());
+    
+    parameterMapping(spec);
 
     try (SqlSession sqlSession = MyBatisUtil.getSqlSession()) {
       ExperimentTemplateMapper experimentTemplateMapper = 
@@ -159,7 +177,7 @@ public class ExperimentTemplateManager {
           "Unable to parse the experimentTemplate spec: " + e.getMessage());
     }
     // Update cache
-    cachedExperimentTemplates.putIfAbsent(spec.getName(), experimentTemplate);
+    cachedExperimentTemplates.put(spec.getName(), experimentTemplate);
 
     return experimentTemplate;
   }
@@ -239,7 +257,7 @@ public class ExperimentTemplateManager {
           ExperimentTemplate tpl = new ExperimentTemplate();
 
           tpl.setExperimentTemplateSpec(
-                new Gson().fromJson(experimentTemplateEntity.getExperimentTemplateSpec(), 
+                gson.fromJson(experimentTemplateEntity.getExperimentTemplateSpec(), 
                 ExperimentTemplateSpec.class));
           tpls.add(tpl);
           cachedExperimentTemplates.put(tpl.getExperimentTemplateSpec().getName(), tpl);
@@ -277,7 +295,7 @@ public class ExperimentTemplateManager {
       if (experimentTemplateEntity != null) {
         tpl = new ExperimentTemplate();
         tpl.setExperimentTemplateSpec(
-            new Gson().fromJson(experimentTemplateEntity.getExperimentTemplateSpec(), 
+            gson.fromJson(experimentTemplateEntity.getExperimentTemplateSpec(), 
             ExperimentTemplateSpec.class));
       }
     } catch (Exception e) {
@@ -306,8 +324,8 @@ public class ExperimentTemplateManager {
 
     ExperimentTemplate experimentTemplate = getExperimentTemplate(SubmittedParam.getName());
     Map<String, String> params = SubmittedParam.getParams();
-
-
+    
+    
     for (ExperimentTemplateParamSpec paramSpec: 
           experimentTemplate.getExperimentTemplateSpec().getExperimentTemplateParamSpec()) {
 
@@ -316,15 +334,50 @@ public class ExperimentTemplateManager {
         paramSpec.setValue(value);
       }
     }
-    String spec = new Gson().toJson(experimentTemplate.getExperimentTemplateSpec());
+    ExperimentTemplateSpec spec = experimentTemplate.getExperimentTemplateSpec();
 
-    ExperimentTemplateSpec experimentTemplateSpec = parameterMapping(spec, params);
-        
-    return ExperimentManager.getInstance().createExperiment(experimentTemplateSpec.getExperimentSpec());
+    ExperimentSpec experimentSpec = parameterMapping(spec, params);
+    
+    return ExperimentManager.getInstance().createExperiment(experimentSpec);
+  }
+  
+
+  private ExperimentTemplateSpec addResourcesParameter(ExperimentTemplateSpec tplSpec) {
+    
+    for (Map.Entry<String, ExperimentTaskSpec> entrySet : tplSpec.getExperimentSpec().getSpec().entrySet()) {
+
+      ExperimentTaskSpec taskSpec = entrySet.getValue();
+      // parse resourceMap
+      taskSpec.setResources(taskSpec.getResources());
+      
+      ExperimentTemplateParamSpec parm1 = new ExperimentTemplateParamSpec();
+      parm1.setName(String.format("spec.%s.replicas", entrySet.getKey()));
+      parm1.setValue(taskSpec.getReplicas() == null ? "1" : taskSpec.getReplicas().toString());
+      parm1.setRequired("false");
+      parm1.setDescription("");
+      tplSpec.getExperimentTemplateParamSpec().add(parm1);
+
+      ExperimentTemplateParamSpec parm2 = new ExperimentTemplateParamSpec();
+      parm2.setName(String.format("spec.%s.resourceMap.cpu", entrySet.getKey()));
+      parm2.setValue(taskSpec.getCpu() == null ? "1" : taskSpec.getCpu());
+      parm2.setRequired("false");
+      parm2.setDescription("");
+      tplSpec.getExperimentTemplateParamSpec().add(parm2);
+
+      ExperimentTemplateParamSpec parm3 = new ExperimentTemplateParamSpec();
+      parm3.setName(String.format("spec.%s.resourceMap.memory", entrySet.getKey()));
+      parm3.setValue(taskSpec.getMemory() == null ? "1" : taskSpec.getMemory().toString());
+      parm3.setRequired("false");
+      parm3.setDescription("");
+      tplSpec.getExperimentTemplateParamSpec().add(parm3);
+
+    }
+    return tplSpec;
   }
 
-  private ExperimentTemplateSpec parameterMapping(String spec) {
-    ExperimentTemplateSpec tplSpec = new Gson().fromJson(spec, ExperimentTemplateSpec.class);
+
+  // use template default value to mapping
+  private ExperimentSpec parameterMapping(ExperimentTemplateSpec tplSpec) {
 
     Map<String, String> paramMap = new HashMap<String, String>();
     for (ExperimentTemplateParamSpec parm : tplSpec.getExperimentTemplateParamSpec()) {
@@ -334,49 +387,132 @@ public class ExperimentTemplateManager {
         paramMap.put(parm.getName(), "");
       }
     }
-
-    return parameterMapping(spec, paramMap);
+    return parameterMapping(tplSpec, paramMap);
   }
 
-  // Use params to replace the content of spec
-  private ExperimentTemplateSpec parameterMapping(String spec, Map<String, String> paramMap) {
-
-    Pattern pattern = Pattern.compile("\\{\\{(.+?)\\}\\}");
-    StringBuffer sb = new StringBuffer();
-    Matcher matcher = pattern.matcher(spec);
-
-    List<String> unmappedKeys = new ArrayList<String>();
-
-    while (matcher.find()) {
-      final String key = matcher.group(1);
-      final String replacement = paramMap.get(key);
-      if (replacement == null) {
-        unmappedKeys.add(key);
-      }
-      else {
-        matcher.appendReplacement(sb, replacement);
-      }
-      paramMap.remove(key);
+  private ExperimentSpec parameterMapping(ExperimentTemplateSpec tplspec, Map<String, String> paramMap) {
+        
+    String spec = gson.toJson(tplspec.getExperimentSpec());
+    Map<String, Object> flattenJson = JsonFlattener.flattenAsMap(spec);
+    
+    Log.info(flattenJson.toString());
+    // illegalParamList: The parameters not in template parameters should not be used
+    // Check at submission
+    Map<String, ExperimentTemplateParamSpec> tplparamMap = new HashMap<String, ExperimentTemplateParamSpec>();
+    for (ExperimentTemplateParamSpec tplParam : tplspec.getExperimentTemplateParamSpec()) {
+      tplparamMap.put(tplParam.getName(), tplParam);
     }
-    matcher.appendTail(sb);
-
-    if (paramMap.size() > 0) {
+    Set<String> illegalParamList = new HashSet<String>();
+    for (String key : paramMap.keySet()) {
+      if (tplparamMap.get(key) == null) {
+        illegalParamList.add(key);
+      }
+    }
+    
+    if (illegalParamList.size() > 0) {
       throw new SubmarineRuntimeException(Status.BAD_REQUEST.getStatusCode(),
-            "Parameters contains unused key: " + paramMap.keySet());
+            "Parameters contains illegal key: " + illegalParamList.toString());
+    } 
+    
+    // unmapedParamList: Parameters that should be used in the template but could not be found
+    // Check at registration and submission
+    Set<String> unmapedParamList = new HashSet<String>();
+    for (ExperimentTemplateParamSpec tplParam : tplspec.getExperimentTemplateParamSpec()) {
+      if (paramMap.get(tplParam.getName()) == null) {
+        // use default value
+        if (!Boolean.parseBoolean(tplParam.getRequired())) {
+          paramMap.put(tplParam.getName(), tplParam.getValue());
+        } else {
+          unmapedParamList.add(tplParam.getName());
+        }
+      }
+    }
+   
+    // unusedParamList: Parameters not used by the template
+    // Check at registration
+    Set<String> unusedParamList = new HashSet<String>();
+    for (String s : paramMap.keySet()) {
+      unusedParamList.add(s);
     }
 
-    if (unmappedKeys.size() > 0) {
+    // resourceMap needs special handling
+    for (Map.Entry<String, ExperimentTaskSpec> entrySet : tplspec.getExperimentSpec().getSpec().entrySet()) {
+      String cpu = paramMap.get(String.format("spec.%s.resourceMap.cpu", entrySet.getKey()));
+      String memory = paramMap.get(String.format("spec.%s.resourceMap.memory", entrySet.getKey()));
+      flattenJson.put(String.format("spec.%s.resources", entrySet.getKey()), 
+            String.format("cpu=%s,memory=%s", cpu, memory));
+    }
+
+    // Mapping the {{...}} param
+    Pattern pattern = Pattern.compile("\\{\\{(.*?)\\}\\}");
+    for (Map.Entry<String, Object> entry : flattenJson.entrySet()) {
+      boolean isMatch = false;
+      if (entry.getValue() instanceof String) {
+        String value = (String) entry.getValue();
+        Matcher matcher = pattern.matcher(value);
+        StringBuffer sb = new StringBuffer();
+
+        while (matcher.find()) {
+          String name = matcher.group(1);
+          String key = entry.getKey() + ":" + name;
+          
+          // match path+placeholder  ("meta.cmd:parametername")
+          if (paramMap.get(key) != null) {
+            isMatch = true;
+            matcher.appendReplacement(sb, paramMap.get(key));
+            unusedParamList.remove(key);
+            unmapedParamList.remove(key);
+          }
+          // match placeholder ("parametername")
+          else if (paramMap.get(name) != null) {
+            isMatch = true;
+            matcher.appendReplacement(sb, paramMap.get(name));
+            unusedParamList.remove(name);
+            unmapedParamList.remove(name);
+          } else {
+            unmapedParamList.add(key);
+          }
+        }
+        if (isMatch) {
+          matcher.appendTail(sb);
+          flattenJson.put(entry.getKey(), sb.toString());
+        }
+      }
+      // match path ("meta.cmd")
+      if (!isMatch) {
+        String key = entry.getKey();
+        if (paramMap.get(key) != null) {
+          flattenJson.put(key, paramMap.get(key));
+          unusedParamList.remove(key);
+        }
+      }
+    }
+
+    if (unusedParamList.size() > 0) {
       throw new SubmarineRuntimeException(Status.BAD_REQUEST.getStatusCode(),
-          "Template contains unmapped key: " + unmappedKeys);
+            "Parameters contains unused key: " + unusedParamList.toString());
+    }
+
+    if (unmapedParamList.size() > 0) {
+      throw new SubmarineRuntimeException(Status.BAD_REQUEST.getStatusCode(),
+          "Template contains unmapped value: " + unmapedParamList.toString());
     }  
-    ExperimentTemplateSpec experimentTemplateSpec;
 
+    String json = flattenJson.toString();
+    Log.info("flattenJson    " + json);
+
+    String nestedJson = JsonUnflattener.unflatten(json);
+    Log.info("nestedJson    " + nestedJson);
+    
+    ExperimentSpec returnExperimentSpec = null;
     try {
-      experimentTemplateSpec = new Gson().fromJson(sb.toString(), ExperimentTemplateSpec.class);
+      returnExperimentSpec = gson.fromJson(nestedJson, ExperimentSpec.class);
+      Log.info("ExperimentSpec " + returnExperimentSpec.toString());
+
     } catch (Exception e) {
       throw new SubmarineRuntimeException(Status.BAD_REQUEST.getStatusCode(),
-          "Template mapping fail: " + e.getMessage() + sb.toString());
+          "Template mapping fail: " + e.getMessage() + nestedJson);
     }
-    return experimentTemplateSpec;
+    return returnExperimentSpec;
   }
 }
