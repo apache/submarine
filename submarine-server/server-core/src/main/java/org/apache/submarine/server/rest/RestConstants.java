084ee513d36f249aa7ec55c06ee54530277b14ce
ByronHsu <byronhsu1230@gmail.com>
2021-02-05 21:44:35 +0800

    update readme

--- submarine-server/server-core/src/main/java/org/apache/submarine/server/rest/RestConstants.java
+++ submarine-server/server-core/src/main/java/org/apache/submarine/server/rest/RestConstants.java
@@ -70,4 +70,13 @@ public class RestConstants {
 
   public static final String NOTEBOOK_ID = "id";
 
+<<<<<<< HEAD
+=======
+  /**
+   * Tensorboard
+   */
+  public static final String LOG_DIR_KEY = "SUBMARINE_LOG_DIR";
+  public static final String LOG_DIR_VALUE = "/logs/mylog";
+
+>>>>>>> f354285... add prefix
 }
