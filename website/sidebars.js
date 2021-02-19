f4a75fc8435b16bcd6d3f437de1bc68ea1822e12
ByronHsu <byronhsu1230@gmail.com>
2021-02-19 14:48:30 +0800

    Add new development guide (#516)
    Byron Hsu via Zhankun Tang

--- website/sidebars.js
+++ website/sidebars.js
@@ -18,104 +18,102 @@
  */
 
 module.exports = {
-  docs: [
-    {
-  	  'Introduction': [],
-  	  'Getting Started': [
-	  	'gettingStarted/localDeployment',
-	  	'gettingStarted/notebook',
-          'gettingStarted/python-sdk',
-      ],
-  	  'User Docs': [
-	  	{
-	  		'Submarine on Kubernetes': [
-	  		    'userDocs/k8s/run-experiment-template-rest',
-	  		    'userDocs/k8s/run-pytorch-experiment-rest',
-	  		    'userDocs/k8s/run-tensorflow-experiment-rest',
-	  		]
-	  	},
-	  	{
-	  		'Submarine on Yarn': [
-	  			'userDocs/yarn/YARNRuntimeGuide',
-	  		    'userDocs/yarn/TestAndTroubleshooting',
-	  		    'userDocs/yarn/Dockerfiles',
-	  		    'userDocs/yarn/WriteDockerfileMX',
-	  		    'userDocs/yarn/WriteDockerfilePT',
-	  		    'userDocs/yarn/WriteDockerfileTF',
-	  		]
-	  	},
-	  	{
-	  		'Submarine SDK': [
-	  	 		'userDocs/submarine-sdk/pysubmarine/development',
-	  	        'userDocs/submarine-sdk/pysubmarine/tracking',
-	  	    ]
-	  	},
-	  	{
-	  		'Submarine Security': [
-	  			'userDocs/submarine-security/spark-security/README',
-                'userDocs/submarine-security/spark-security/build-submarine-spark-security-plugin',
-            ]
+    docs: [
+        {
+            Introduction: [],
+            "Getting Started": [
+                "gettingStarted/localDeployment",
+                "gettingStarted/notebook",
+                "gettingStarted/python-sdk",
+            ],
+            "User Docs": [
+                {
+                    "Submarine on Kubernetes": [
+                        "userDocs/k8s/run-experiment-template-rest",
+                        "userDocs/k8s/run-pytorch-experiment-rest",
+                        "userDocs/k8s/run-tensorflow-experiment-rest",
+                    ],
+                },
+                {
+                    "Submarine on Yarn": [
+                        "userDocs/yarn/YARNRuntimeGuide",
+                        "userDocs/yarn/TestAndTroubleshooting",
+                        "userDocs/yarn/Dockerfiles",
+                        "userDocs/yarn/WriteDockerfileMX",
+                        "userDocs/yarn/WriteDockerfilePT",
+                        "userDocs/yarn/WriteDockerfileTF",
+                    ],
+                },
+                {
+                    "Submarine SDK": [
+                        "userDocs/submarine-sdk/pysubmarine/development",
+                        "userDocs/submarine-sdk/pysubmarine/tracking",
+                    ],
+                },
+                {
+                    "Submarine Security": [
+                        "userDocs/submarine-security/spark-security/README",
+                        "userDocs/submarine-security/spark-security/build-submarine-spark-security-plugin",
+                    ],
+                },
+            ],
+            "Administrator Docs": [
+                {
+                    "Submarine on Kubernetes": [
+                        "adminDocs/k8s/README",
+                        "adminDocs/k8s/kind",
+                        "adminDocs/k8s/helm",
+                    ],
+                },
+                {
+                    "Submarine on Yarn": ["adminDocs/yarn/README"],
+                },
+            ],
+            "Developer Docs": [
+                "devDocs/README",
+                "devDocs/BuildFromCode",
+                "devDocs/Development",
+                "devDocs/IntegrationTest",
+            ],
+            Community: [
+                "community/README",
+                "community/HowToCommit",
+                "community/contributing",
+            ],
+            "Design Docs": [
+                "designDocs/architecture-and-requirements",
+                "designDocs/implementation-notes",
+                "designDocs/environments-implementation",
+                "designDocs/experiment-implementation",
+                "designDocs/notebook-implementation",
+                "designDocs/storage-implementation",
+                {
+                    "Submarine Server": [
+                        "designDocs/submarine-server/architecture",
+                        "designDocs/submarine-server/experimentSpec",
+                    ],
+                },
+                {
+                    "WIP Design Docs": [
+                        "designDocs/wip-designs/submarine-launcher",
+                        "designDocs/wip-designs/submarine-clusterServer",
+                        "designDocs/wip-designs/security-implementation",
+                    ],
+                },
+            ],
+            Releases: [
+                "releases/submarine-release-0.2.0",
+                "releases/submarine-release-0.3.0",
+                "releases/submarine-release-0.4.0",
+                "releases/submarine-release-0.5.0",
+            ],
+            RoadMap: [],
         },
-      ],
-  	  'Administrator Docs': [
-	  	{
-	  		'Submarine on Kubernetes': [
-               	'adminDocs/k8s/README',
-	  		 	'adminDocs/k8s/kind',
-	  		 	'adminDocs/k8s/helm',
-	  		]
-	  	},
-	  	{
-	  		'Submarine on Yarn': [
-	  		   'adminDocs/yarn/README',
-	  		]
-	  	},
-  	  ],
-  	  'Developer Docs': [
-	  	'devDocs/README',
-	  	'devDocs/BuildFromCode',
-	  	'devDocs/IntegrationTest',
-	  	'devDocs/QuickDevTips',
-	  ],
-  	  'Community': [
-	  	 'community/README',
-	  	 'community/HowToCommit',
-	  	 'community/contributing',
-  	  ],
-	  'Design Docs': [
-	  	'designDocs/architecture-and-requirements',
-	      'designDocs/implementation-notes',
-          'designDocs/environments-implementation',
-          'designDocs/experiment-implementation',
-          'designDocs/notebook-implementation',
-          'designDocs/storage-implementation',
-          {
-          	'Submarine Server': [
-              	'designDocs/submarine-server/architecture',
-                  'designDocs/submarine-server/experimentSpec',
-                  ]
-          },
-          {
-          	'WIP Design Docs': [
-          		'designDocs/wip-designs/submarine-launcher',
-          		'designDocs/wip-designs/submarine-clusterServer',
-          		'designDocs/wip-designs/security-implementation',
-          		]
-          },
-      ],
-		'Releases': [
-			'releases/submarine-release-0.2.0',
-			'releases/submarine-release-0.3.0',
-			'releases/submarine-release-0.4.0',
-			'releases/submarine-release-0.5.0',
-		],
-		'RoadMap': [],
-    },
-  ],
-  api: [
-  	'api/environment',
-  	'api/experiment',
-  	'api/experiment-template',
-  	'api/notebook',
-  ],
+    ],
+    api: [
+        "api/environment",
+        "api/experiment",
+        "api/experiment-template",
+        "api/notebook",
+    ],
 };
