1c5e3cb8c0424882b6eb1fe32604c5148235346b
Kai-Hsun Chen <b03901153@ntu.edu.tw>
2021-03-24 17:11:03 +0800

    SUBMARINE-770. Create Submarine CRD & Test API generated by code-generator
    ### What is this PR for?
    This task aims to test API generated by code-generator. (SUBMARINE-768)
    To achieve this, we should follow the following steps
    
    Step1: Register Custom Resource Defintion (CRD) "Submarine"
    Step2: Create a Custom Resource for CRD Submarine
    Step3: Test API generated by code-generator.
    
    ### What type of PR is it?
    [Feature]
    
    ### Todos
    * Implement controller pattern
    
    ### What is the Jira issue?
    https://issues.apache.org/jira/browse/SUBMARINE-770
    
    ### How should this be tested?
    ```
    # Step0: Move to submarine-cloud-v2/
    
    # Step1: Register Custom Resource Definition
    kubectl apply -f artifacts/examples/crd.yaml
    
    # Step2: Create a Custom Resource
    kubectl apply -f artifacts/examples/example-submarine.yaml
    
    # Step3: Run unit test
    go test
    ```
    
    ### Screenshots (if appropriate)
    
    https://user-images.githubusercontent.com/20109646/112088423-c0d87d00-8bca-11eb-962a-19d7f13e3a49.mov
    
    ### Questions:
    * Does the licenses files need update? No
    * Is there breaking changes for older versions? No
    * Does this needs documentation? Yes
    
    Author: Kai-Hsun Chen <b03901153@ntu.edu.tw>
    
    Signed-off-by: Liu Xun <liuxun@apache.org>
    
    Closes #541 from kevin85421/SUBMARINE-770 and squashes the following commits:
    
    037b90a [Kai-Hsun Chen] Fix: indentation
    662d4e9 [Kai-Hsun Chen] Fix: indentation
    7a355f2 [Kai-Hsun Chen] Fix: Redeclare masterURL and kubeconfig
    be5357f [Kai-Hsun Chen] SUBMARINE-770. Create Submarine CRD & Test API generated by code-generator

---
+++ submarine-cloud-v2/main.go
@@ -0,0 +1,41 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package main
+
+import (
+	"flag"
+	"k8s.io/klog/v2"
+	"os"
+)
+
+var (
+	masterURL  string
+	kubeconfig string
+)
+
+func main() {
+	klog.InitFlags(nil)
+	flag.Parse()
+
+	// TODO: Create a Submarine operator	
+}
+
+func init() {
+	flag.StringVar(&kubeconfig, "kubeconfig", os.Getenv("HOME") + "/.kube/config", "Path to a kubeconfig. Only required if out-of-cluster.")
+	flag.StringVar(&masterURL, "master", "", "The address of the Kubernetes API server. Overrides any value in kubeconfig. Only required if out-of-cluster.")
+}
