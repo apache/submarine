97547d49acf850dec26c35507a5bd98688782f27
kobe860219 <kobe860219@gmail.com>
2021-04-22 14:32:40 +0800

    minio server done

---
+++ dev-support/docker-images/mlflow/set.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+#
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# description: Start and stop daemon script for.
+#
+
+set -euo pipefail
+
+/bin/bash -c "sleep 60; ./mc config host add minio http://10.96.0.4:9000 submarine_minio submarine_minio"
+
+/bin/bash -c "./mc mb minio/mlflow"
