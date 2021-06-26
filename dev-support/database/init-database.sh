0b4006330875ddf5bf633f59bd4344c77576fe24
jeff-901 <b07901052@ntu.edu.com>
2021-06-26 13:26:32 +0800

    add mysql for mlflow backend

--- dev-support/database/init-database.sh
+++ dev-support/database/init-database.sh
@@ -38,3 +38,8 @@ mysql -e "CREATE DATABASE IF NOT EXISTS metastore;"
 mysql -e "CREATE USER IF NOT EXISTS 'metastore'@'%' IDENTIFIED BY 'password';"
 mysql -e "GRANT ALL PRIVILEGES ON *.* TO 'metastore'@'%';"
 mysql -e "use metastore; source ./metastore.sql; show tables;"
+
+mysql -e "CREATE DATABASE IF NOT EXISTS mlflow;"
+mysql -e "CREATE USER IF NOT EXISTS 'mlflow'@'%' IDENTIFIED BY 'password';"
+mysql -e "GRANT ALL PRIVILEGES ON *.* TO 'mlflow'@'%';"
+mysql -e "use mlflow; source ./mlflow.sql; show tables;"
