fb9d6a6f46c04c2581bdcde3482bc61842cfd554
ByronHsu <byronhsu1230@gmail.com>
2021-06-28 14:41:37 +0800

    monitor error

--- .github/scripts/build-image-locally.sh
+++ .github/scripts/build-image-locally.sh
@@ -17,7 +17,7 @@
 #
 
 SUBMARINE_VERSION="0.6.0-SNAPSHOT"
-# IMAGE_LIST=("database" "jupyter" "mlflow" "submarine")
+# IMAGE_LIST=("database" "mlflow" "submarine")
 IMAGE_LIST=("submarine")
 
 for image in "${IMAGE_LIST[@]}"
