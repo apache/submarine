e491f8ccf7f96755ab2576020c9aa2b87ac57e6a
jeff-901 <b07901052@ntu.edu.com>
2021-07-03 13:44:53 +0800

    delete by mlflow_id

--- submarine-server/server-core/src/main/java/org/apache/submarine/server/rest/ExperimentRestApi.java
+++ submarine-server/server-core/src/main/java/org/apache/submarine/server/rest/ExperimentRestApi.java
@@ -31,6 +31,7 @@ import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.util.List;
+import java.util.Optional;
 
 import com.google.common.annotations.VisibleForTesting;
 
@@ -60,6 +61,8 @@ import org.mlflow.tracking.MlflowClient;
 @Produces({MediaType.APPLICATION_JSON + "; " + RestConstants.CHARSET_UTF8})
 public class ExperimentRestApi {
   private ExperimentManager experimentManager = ExperimentManager.getInstance();
+  private Optional<org.mlflow.api.proto.Service.Experiment> MlflowExperimentOptional;
+  private org.mlflow.api.proto.Service.Experiment MlflowExperiment;
 
   @VisibleForTesting
   public void setExperimentManager(ExperimentManager experimentManager) {
@@ -223,10 +226,14 @@ public class ExperimentRestApi {
     } catch (SubmarineRuntimeException e) {
       return parseExperimentServiceException(e);
     }
-    MlflowClient mlflowclient = MlflowClient("http://submarine-mlflow-service:5000");
-    mlflowclient.deleteExperiment(id);
+    MlflowClient mlflowClient = new MlflowClient("http://submarine-mlflow-service:5000");
+    MlflowExperimentOptional = mlflowClient.getExperimentByName(id);
+    MlflowExperiment = MlflowExperimentOptional.get();
+    String mlflowId = MlflowExperiment.getExperimentId();
+    
+    mlflowClient.deleteExperiment(mlflowId);
     return new JsonResponse.Builder<Experiment>(Response.Status.OK).success(true)
-            .result(experiment).build();
+          .result(experiment).build();
   }
 
   @GET
