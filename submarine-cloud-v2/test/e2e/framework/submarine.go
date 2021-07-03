efaa226077e1f976bcda112a5683442479243158
Kenchu123 <k88913n@gmail.com>
2021-07-03 17:50:48 +0800

    SUBMARINE-850. fix framework submarine bug and add licence

--- submarine-cloud-v2/test/e2e/framework/submarine.go
+++ submarine-cloud-v2/test/e2e/framework/submarine.go
@@ -1,3 +1,20 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package framework
 
 import (
@@ -40,15 +57,15 @@ func UpdateSubmarine(clientset clientset.Interface, namespace string, submarine 
 }
 
 func GetSubmarine(clientset clientset.Interface, namespace string, name string) (*v1alpha1.Submarine, error) {
-	submarine, err := clientset.SubmarineV1alpha1().Submarines(namespace).Get(context.TODO(), name, metav1.GetOptinos{})
+	submarine, err := clientset.SubmarineV1alpha1().Submarines(namespace).Get(context.TODO(), name, metav1.GetOptions{})
 	if err != nil {
 		return nil, err
 	}
 	return submarine, nil
 }
 
-func DeleteSubmarine(clientset clientset.Interface, namespace string, submarine *v1alpha1.Submarine) error {
-	_, err := clientset.SubmarineV1alpha1().Submarines(namespace).Delete(context.TODO(), submarine, metav1.DeleteOptions{})
+func DeleteSubmarine(clientset clientset.Interface, namespace string, name string) error {
+	err := clientset.SubmarineV1alpha1().Submarines(namespace).Delete(context.TODO(), name, metav1.DeleteOptions{})
 	if err != nil {
 		return err
 	}
