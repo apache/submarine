fcb5cdc2e77a5c695cf8169430cd67eb08a43647
jeff-901 <b07901052@ntu.edu.tw>
2021-08-18 20:53:53 +0800

    delete unused file

--- bin/install.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-helm install submarine ./helm-charts/submarine
-echo ""
-kubectl create ns seldon-system
-echo ""
-helm install seldon-core seldon-core-operator \
-    --repo https://storage.googleapis.com/seldon-charts \
-    --namespace seldon-system 2> /dev/null
