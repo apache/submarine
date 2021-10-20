fc4389d6e90e5ecbb87774537f1e0a18e18c4493
jeff-901 <b07901052@ntu.edu.tw>
2021-10-20 11:36:51 +0800

    move serve to submarine-serve

--- submarine-serve/installation/install.sh
+++ submarine-serve/installation/install.sh
@@ -14,24 +14,38 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-helm install submarine ./helm-charts/submarine
+set -euxo pipefail
 
-if [[ ! -f ./dev-support/installation/istioctl ]]; then
+if [ -L ${BASH_SOURCE-$0} ]; then
+  PWD=$(dirname $(readlink "${BASH_SOURCE-$0}"))
+else
+  PWD=$(dirname ${BASH_SOURCE-$0})
+fi
+export CURRENT_PATH=$(cd "${PWD}">/dev/null; pwd)
+SUBMARINE_HOME=${CURRENT_PATH}/../..
+
+echo "submarine: $SUBMARINE_HOME"
+
+helm install submarine ${SUBMARINE_HOME}/helm-charts/submarine
+
+INSTALLATION_HOME=""
+
+if [[ ! -f ${CURRENT_PATH}/istioctl ]]; then
     wget https://github.com/istio/istio/releases/download/1.6.8/istio-1.6.8-linux-amd64.tar.gz
     tar zxvf istio-1.6.8-linux-amd64.tar.gz
     rm -rf istio-1.6.8-linux-amd64.tar.gz
-    cp ./istio-1.6.8/bin/istioctl ./dev-support/installation/istioctl
+    cp ./istio-1.6.8/bin/istioctl ${CURRENT_PATH}/istioctl
     rm -rf istio-1.6.8
 fi
 
-./dev-support/installation/istioctl install --skip-confirmation
+${CURRENT_PATH}/istioctl install --skip-confirmation
 
 kubectl create ns seldon-system
-kubectl apply -f ./dev-support/installation/seldon-secret.yaml
+kubectl apply -f ${CURRENT_PATH}/seldon-secret.yaml
 helm install seldon-core seldon-core-operator \
     --repo https://storage.googleapis.com/seldon-charts \
     --namespace seldon-system \
     --version 1.10.0 \
     --set istio.enabled=true \
     --set executor.defaultEnvSecretRefName=seldon-core-init-container-secret 2> /dev/null
-kubectl apply -f ./dev-support/installation/seldon-gateway.yaml
+kubectl apply -f ${CURRENT_PATH}/seldon-gateway.yaml
