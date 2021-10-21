e588327342b860c06de40751357f41bbaf8e4e84
woodcutter-eric <yingchiao.chen@gmail.com>
2021-10-21 23:01:22 +0800

    SUBMARINE-1055. Use builder pattern to simplify construction of controller
    ### What is this PR for?
    <!-- A few sentences describing the overall goals of the pull request's commits.
    First time? Check out the contributing guide - https://submarine.apache.org/contribution/contributions.html
    -->
    
    Now, the construction of controller is bulky. It needs a lot of arguments when making a new instance. We may use builder design pattern to solve this problem to make code more cleaner.
    
    ### What type of PR is it?
    [Improvement | Refactoring]
    
    ### Todos
    * [x] - Use builder pattern to reduce the number of parameters of NewController
    * [x] - Replace NewController with a higher level wrapper function
    * [x] - Partition initialization logic in the creation of a controller
    * [x] - Decouple event register and setup
    * [x] - Add interfaces to restrict access to other internal functions
    
    ### What is the Jira issue?
    <!-- * Open an issue on Jira https://issues.apache.org/jira/browse/SUBMARINE/
    * Put link here, and add [SUBMARINE-*Jira number*] in PR title, eg. `SUBMARINE-23. PR title`
    -->
    
    https://issues.apache.org/jira/projects/SUBMARINE/issues/SUBMARINE-1055
    
    ### How should this be tested?
    <!--
    * First time? Setup Travis CI as described on https://submarine.apache.org/contribution/contributions.html#continuous-integration
    * Strongly recommended: add automated unit tests for any new or changed behavior
    * Outline any manual steps to test the PR here.
    -->
    ### Screenshots (if appropriate)
    
    ### Questions:
    * Do the license files need updating? No
    * Are there breaking changes for older versions? No
    * Does this need new documentation? No
    
    Author: woodcutter-eric <yingchiao.chen@gmail.com>
    
    Signed-off-by: Kevin <pingsutw@apache.org>
    
    Closes #780 from woodcutter-eric/SUBMARINE-1055 and squashes the following commits:
    
    9721794f [woodcutter-eric] SUBMARINE-1055. Add several builder event handler functions to internal
    97caf778 [woodcutter-eric] SUBMARINE-1055. Add several builder functions to internal
    3615ec98 [woodcutter-eric] SUBMARINE-1055. Fix controller builder design and typos
    0093b37d [woodcutter-eric] SUBMARINE-1055. Add interfaces for controller and builder
    15a75085 [woodcutter-eric] SUBMARINE-1055. Add license header for new files
    53857b39 [woodcutter-eric] SUBMARINE-1055. Replace the original controller creatiion process
    6467430b [woodcutter-eric] SUBMARINE-1055. Remove NewController() function
    a0e4db40 [woodcutter-eric] SUBMARINE-1055. Refactor controller event handlers
    453c5ba6 [woodcutter-eric] SUBMARINE-1055. Add controller builder

---
+++ submarine-cloud-v2/pkg/controller/controller_builder.go
@@ -0,0 +1,115 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controller
+
+import (
+	submarinescheme "github.com/apache/submarine/submarine-cloud-v2/pkg/client/clientset/versioned/scheme"
+	corev1 "k8s.io/api/core/v1"
+	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
+	"k8s.io/client-go/kubernetes/scheme"
+	typedcorev1 "k8s.io/client-go/kubernetes/typed/core/v1"
+	"k8s.io/client-go/tools/record"
+	"k8s.io/client-go/util/workqueue"
+	"k8s.io/klog/v2"
+)
+
+type ControllerBuilder struct {
+	controller *Controller
+	config     *BuilderConfig
+}
+
+func NewControllerBuilder(config *BuilderConfig) *ControllerBuilder {
+	return &ControllerBuilder{
+		controller: &Controller{},
+		config:     config,
+	}
+}
+
+func (cb *ControllerBuilder) Build() *Controller {
+	cb.initialize()
+	cb.addClientsets()
+	cb.addListers()
+	cb.addEventHandlers()
+
+	return cb.controller
+}
+
+func (cb *ControllerBuilder) initialize() *ControllerBuilder {
+	// Add Submarine types to the default Kubernetes Scheme so Events can be
+	// logged for Submarine types.
+	utilruntime.Must(submarinescheme.AddToScheme(scheme.Scheme))
+	klog.V(4).Info("Creating event broadcaster")
+
+	eventBroadcaster := record.NewBroadcaster()
+	eventBroadcaster.StartStructuredLogging(0)
+	eventBroadcaster.StartRecordingToSink(&typedcorev1.EventSinkImpl{Interface: cb.config.kubeclientset.CoreV1().Events("")})
+	recorder := eventBroadcaster.NewRecorder(scheme.Scheme, corev1.EventSource{Component: controllerAgentName})
+
+	workqueue := workqueue.NewNamedRateLimitingQueue(workqueue.DefaultControllerRateLimiter(), "Submarines")
+
+	cb.controller.incluster = cb.config.incluster
+	cb.controller.recorder = recorder
+	cb.controller.workqueue = workqueue
+
+	return cb
+}
+
+func (cb *ControllerBuilder) addClientsets() *ControllerBuilder {
+	cb.controller.kubeclientset = cb.config.kubeclientset
+	cb.controller.submarineclientset = cb.config.submarineclientset
+	cb.controller.traefikclientset = cb.config.traefikclientset
+
+	return cb
+}
+
+func (cb *ControllerBuilder) addListers() *ControllerBuilder {
+	cb.controller.submarinesLister = cb.config.submarineInformer.Lister()
+	cb.controller.submarinesSynced = cb.config.submarineInformer.Informer().HasSynced
+
+	cb.controller.deploymentLister = cb.config.deploymentInformer.Lister()
+	cb.controller.namespaceLister = cb.config.namespaceInformer.Lister()
+	cb.controller.serviceLister = cb.config.serviceInformer.Lister()
+	cb.controller.serviceaccountLister = cb.config.serviceaccountInformer.Lister()
+	cb.controller.persistentvolumeclaimLister = cb.config.persistentvolumeclaimInformer.Lister()
+	cb.controller.ingressLister = cb.config.ingressInformer.Lister()
+	cb.controller.ingressrouteLister = cb.config.ingressrouteInformer.Lister()
+	cb.controller.roleLister = cb.config.roleInformer.Lister()
+	cb.controller.rolebindingLister = cb.config.rolebindingInformer.Lister()
+
+	return cb
+}
+
+func (cb *ControllerBuilder) addEventHandlers() *ControllerBuilder {
+	klog.Info("Setting up event handlers")
+
+	// Setting up event handler for Submarine
+	cb.addSubmarineEventHandlers()
+
+	// Setting up event handler for other resources
+	cb.addNamespaceEventHandlers()
+	cb.addDeploymentEventHandlers()
+	cb.addServiceEventHandlers()
+	cb.addServiceAccountEventHandlers()
+	cb.addPersistentVolumeClaimEventHandlers()
+	cb.addIngressEventHandlers()
+	cb.addIngressRouteEventHandlers()
+	cb.addRoleEventHandlers()
+	cb.addRoleBindingEventHandlers()
+
+	return cb
+}
---
+++ submarine-cloud-v2/pkg/controller/controller_event_handlers.go
@@ -0,0 +1,191 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controller
+
+import (
+	traefikv1alpha1 "github.com/traefik/traefik/v2/pkg/provider/kubernetes/crd/traefik/v1alpha1"
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	extensionsv1beta1 "k8s.io/api/extensions/v1beta1"
+	rbacv1 "k8s.io/api/rbac/v1"
+	"k8s.io/client-go/tools/cache"
+)
+
+func (cb *ControllerBuilder) addSubmarineEventHandlers() *ControllerBuilder {
+	cb.config.submarineInformer.Informer().AddEventHandler(cache.ResourceEventHandlerFuncs{
+		AddFunc: cb.controller.enqueueSubmarine,
+		UpdateFunc: func(old, new interface{}) {
+			cb.controller.enqueueSubmarine(new)
+		},
+	})
+
+	return cb
+}
+
+func (cb *ControllerBuilder) addNamespaceEventHandlers() *ControllerBuilder {
+	cb.config.namespaceInformer.Informer().AddEventHandler(cache.ResourceEventHandlerFuncs{
+		AddFunc: cb.controller.handleObject,
+		UpdateFunc: func(old, new interface{}) {
+			newNamespace := new.(*corev1.Namespace)
+			oldNamespace := old.(*corev1.Namespace)
+			if newNamespace.ResourceVersion == oldNamespace.ResourceVersion {
+				return
+			}
+			cb.controller.handleObject(new)
+		},
+		DeleteFunc: cb.controller.handleObject,
+	})
+
+	return cb
+}
+
+func (cb *ControllerBuilder) addDeploymentEventHandlers() *ControllerBuilder {
+	cb.config.deploymentInformer.Informer().AddEventHandler(cache.ResourceEventHandlerFuncs{
+		AddFunc: cb.controller.handleObject,
+		UpdateFunc: func(old, new interface{}) {
+			newDeployment := new.(*appsv1.Deployment)
+			oldDeployment := old.(*appsv1.Deployment)
+			if newDeployment.ResourceVersion == oldDeployment.ResourceVersion {
+				return
+			}
+			cb.controller.handleObject(new)
+		},
+		DeleteFunc: cb.controller.handleObject,
+	})
+
+	return cb
+}
+
+func (cb *ControllerBuilder) addServiceEventHandlers() *ControllerBuilder {
+	cb.config.serviceInformer.Informer().AddEventHandler(cache.ResourceEventHandlerFuncs{
+		AddFunc: cb.controller.handleObject,
+		UpdateFunc: func(old, new interface{}) {
+			newService := new.(*corev1.Service)
+			oldService := old.(*corev1.Service)
+			if newService.ResourceVersion == oldService.ResourceVersion {
+				return
+			}
+			cb.controller.handleObject(new)
+		},
+		DeleteFunc: cb.controller.handleObject,
+	})
+
+	return cb
+}
+
+func (cb *ControllerBuilder) addServiceAccountEventHandlers() *ControllerBuilder {
+	cb.config.serviceaccountInformer.Informer().AddEventHandler(cache.ResourceEventHandlerFuncs{
+		AddFunc: cb.controller.handleObject,
+		UpdateFunc: func(old, new interface{}) {
+			newServiceAccount := new.(*corev1.ServiceAccount)
+			oldServiceAccount := old.(*corev1.ServiceAccount)
+			if newServiceAccount.ResourceVersion == oldServiceAccount.ResourceVersion {
+				return
+			}
+			cb.controller.handleObject(new)
+		},
+		DeleteFunc: cb.controller.handleObject,
+	})
+
+	return cb
+}
+
+func (cb *ControllerBuilder) addPersistentVolumeClaimEventHandlers() *ControllerBuilder {
+	cb.config.persistentvolumeclaimInformer.Informer().AddEventHandler(cache.ResourceEventHandlerFuncs{
+		AddFunc: cb.controller.handleObject,
+		UpdateFunc: func(old, new interface{}) {
+			newPVC := new.(*corev1.PersistentVolumeClaim)
+			oldPVC := old.(*corev1.PersistentVolumeClaim)
+			if newPVC.ResourceVersion == oldPVC.ResourceVersion {
+				return
+			}
+			cb.controller.handleObject(new)
+		},
+		DeleteFunc: cb.controller.handleObject,
+	})
+
+	return cb
+}
+
+func (cb *ControllerBuilder) addIngressEventHandlers() *ControllerBuilder {
+	cb.config.ingressInformer.Informer().AddEventHandler(cache.ResourceEventHandlerFuncs{
+		AddFunc: cb.controller.handleObject,
+		UpdateFunc: func(old, new interface{}) {
+			newIngress := new.(*extensionsv1beta1.Ingress)
+			oldIngress := old.(*extensionsv1beta1.Ingress)
+			if newIngress.ResourceVersion == oldIngress.ResourceVersion {
+				return
+			}
+			cb.controller.handleObject(new)
+		},
+		DeleteFunc: cb.controller.handleObject,
+	})
+
+	return cb
+}
+
+func (cb *ControllerBuilder) addIngressRouteEventHandlers() *ControllerBuilder {
+	cb.config.ingressrouteInformer.Informer().AddEventHandler(cache.ResourceEventHandlerFuncs{
+		AddFunc: cb.controller.handleObject,
+		UpdateFunc: func(old, new interface{}) {
+			newIngressRoute := new.(*traefikv1alpha1.IngressRoute)
+			oldIngressRoute := old.(*traefikv1alpha1.IngressRoute)
+			if newIngressRoute.ResourceVersion == oldIngressRoute.ResourceVersion {
+				return
+			}
+			cb.controller.handleObject(new)
+		},
+		DeleteFunc: cb.controller.handleObject,
+	})
+
+	return cb
+}
+
+func (cb *ControllerBuilder) addRoleEventHandlers() *ControllerBuilder {
+	cb.config.roleInformer.Informer().AddEventHandler(cache.ResourceEventHandlerFuncs{
+		AddFunc: cb.controller.handleObject,
+		UpdateFunc: func(old, new interface{}) {
+			newRole := new.(*rbacv1.Role)
+			oldRole := old.(*rbacv1.Role)
+			if newRole.ResourceVersion == oldRole.ResourceVersion {
+				return
+			}
+			cb.controller.handleObject(new)
+		},
+		DeleteFunc: cb.controller.handleObject,
+	})
+
+	return cb
+}
+
+func (cb *ControllerBuilder) addRoleBindingEventHandlers() *ControllerBuilder {
+	cb.config.rolebindingInformer.Informer().AddEventHandler(cache.ResourceEventHandlerFuncs{
+		AddFunc: cb.controller.handleObject,
+		UpdateFunc: func(old, new interface{}) {
+			newRoleBinding := new.(*rbacv1.RoleBinding)
+			oldRoleBinding := old.(*rbacv1.RoleBinding)
+			if newRoleBinding.ResourceVersion == oldRoleBinding.ResourceVersion {
+				return
+			}
+			cb.controller.handleObject(new)
+		},
+		DeleteFunc: cb.controller.handleObject,
+	})
+
+	return cb
+}
