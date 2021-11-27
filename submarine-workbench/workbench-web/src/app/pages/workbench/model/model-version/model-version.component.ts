d844773b43f5129c77215a24a10aca0e3bbca016
Ray02250418 <s960379ray@gmail.com>
2021-11-27 15:30:13 +0800

    SUBMARINE-1098. Static model version page

--- submarine-workbench/workbench-web/src/app/pages/workbench/model/model-version/model-version.component.ts
+++ submarine-workbench/workbench-web/src/app/pages/workbench/model/model-version/model-version.component.ts
@@ -34,6 +34,7 @@ export class ModelVersionComponent implements OnInit {
   modelName;
   modelVersion;
   modelVersionInfo: ModelVersionInfo;
+  test;
 
   constructor(
     private router: Router, 
@@ -45,11 +46,27 @@ export class ModelVersionComponent implements OnInit {
   ngOnInit() {
     this.modelName = this.route.snapshot.params.name;
     this.modelVersion = this.route.snapshot.params.version;
+    this.test = 'test message';
+    this.modelVersionInfo = {
+      'name': "register",
+      'version': "1",
+      'source': "s3://submarine/experiment-1637939541827-0001/example/1",
+      'userId': "test_userId",
+      'experimentId': "experiment-1637939541827-0001",
+      'currentStage': "None",
+      'creationTime': "2021-11-26 15:12:29",
+      'lastUpdatedTime': "2021-11-26 15:12:29",
+      'dataset': null,
+      'description': null,
+      'tags': "123"
+    }
     this.experimentService.emitInfo(this.modelName);
+    console.log(this.modelVersionInfo);
+    /**
     this.modelVersionService.querySpecificModel(this.modelName, this.modelVersion).subscribe(
       (item) => {
         this.modelVersionInfo = item;
       }
-    )
+    )*/
   }
 }
