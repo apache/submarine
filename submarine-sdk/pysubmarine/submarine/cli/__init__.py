8418f3a283b12d2f5bdbbe9e34e1b14f75c1b7dc
atosystem <atosystem@hotmail.com>
2021-12-07 12:38:05 +0800

    SUBMARINE-1095. Remove comments

--- submarine-sdk/pysubmarine/submarine/cli/__init__.py
+++ submarine-sdk/pysubmarine/submarine/cli/__init__.py
@@ -28,27 +28,4 @@
 
 from __future__ import absolute_import
 
-# # import apis into sdk package
-# from submarine.experiment.api.experiment_api import ExperimentApi
-
-# # import ApiClient
-# from submarine.experiment.api_client import ApiClient
-# from submarine.experiment.configuration import Configuration
-# from submarine.experiment.exceptions import (
-#     ApiException,
-#     ApiKeyError,
-#     ApiTypeError,
-#     ApiValueError,
-#     OpenApiException,
-# )
-
-# # import models into sdk package
-# from submarine.experiment.models.code_spec import CodeSpec
-# from submarine.experiment.models.environment_spec import EnvironmentSpec
-# from submarine.experiment.models.experiment_meta import ExperimentMeta
-# from submarine.experiment.models.experiment_spec import ExperimentSpec
-# from submarine.experiment.models.experiment_task_spec import ExperimentTaskSpec
-# from submarine.experiment.models.json_response import JsonResponse
-# from submarine.experiment.models.kernel_spec import KernelSpec
-
 __version__ = "0.7.0-SNAPSHOT"
