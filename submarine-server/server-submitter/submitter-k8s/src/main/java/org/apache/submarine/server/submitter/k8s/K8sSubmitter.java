f6bdb9c30348c544fc7c78a9abd3129494c046eb
noidname01 <tim983649@gmail.com>
2021-12-19 22:19:57 +0800

    final

--- submarine-server/server-submitter/submitter-k8s/src/main/java/org/apache/submarine/server/submitter/k8s/K8sSubmitter.java
+++ submarine-server/server-submitter/submitter-k8s/src/main/java/org/apache/submarine/server/submitter/k8s/K8sSubmitter.java
@@ -550,13 +550,6 @@ public class K8sSubmitter implements Submitter {
               seldonDeployment.getMetadata().getName(),
               null, null, null, null,
               new V1DeleteOptionsBuilder().withApiVersion(seldonDeployment.getApiVersion()).build());
-        api.deleteNamespacedCustomObject(istioVirtualService.getGroup(),
-              istioVirtualService.getVersion(),
-              "default",
-              istioVirtualService.getPlural(),
-              istioVirtualService.getMetadata().getName(),
-              null, null, null, null,
-              new V1DeleteOptionsBuilder().withApiVersion(istioVirtualService.getApiVersion()).build());
       } catch (ApiException e1) {
         LOG.error(e1.getMessage(), e1);
       }
@@ -577,6 +570,13 @@ public class K8sSubmitter implements Submitter {
               seldonDeployment.getPlural(),
               seldonDeployment.getMetadata().getName(), null, null, null,
               null, new V1DeleteOptionsBuilder().withApiVersion(seldonDeployment.getApiVersion()).build());
+        api.deleteNamespacedCustomObject(istioVirtualService.getGroup(),
+                istioVirtualService.getVersion(),
+                "default",
+                istioVirtualService.getPlural(),
+                istioVirtualService.getMetadata().getName(),
+                null, null, null, null,
+                new V1DeleteOptionsBuilder().withApiVersion(istioVirtualService.getApiVersion()).build());
     } catch (ApiException e) {
       LOG.error(e.getMessage(), e);
       throw new SubmarineRuntimeException(e.getCode(), e.getMessage());
