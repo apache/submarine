7ab6b043c86687bb03fe08bc442aa096047f3520
KUAN-HSUN-LI <b06209027@ntu.edu.tw>
2022-01-12 17:58:45 +0800

    SUBMARINE-1157. fix

--- submarine-sdk/pysubmarine/submarine/cli/serve/command.py
+++ submarine-sdk/pysubmarine/submarine/cli/serve/command.py
@@ -113,7 +113,7 @@ def delete_serve(model_name: str, model_version: int, wait: bool):
                 json_data = richJSON.from_data(result)
                 console.print(
                     Panel(
-                        json_data, title=f"Serve: model name:{model_name}, version: {model_version}"
+                        json_data, title=f"[bold green] Serve Deleted: model name:{model_name}, version: {model_version}"
                     )
                 )
     except ApiException as err:
