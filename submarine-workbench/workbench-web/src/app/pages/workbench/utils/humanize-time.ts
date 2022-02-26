c3fba51be813007ceefb786e7b9f0360a5537bcd
Ray02250418 <s960379ray@gmail.com>
2022-02-26 11:26:43 +0800

    SUNMARINE-1158. Bug fix

--- submarine-workbench/workbench-web/src/app/pages/workbench/utils/humanize-time.ts
+++ submarine-workbench/workbench-web/src/app/pages/workbench/utils/humanize-time.ts
@@ -4,24 +4,24 @@ export function humanizeTime(time: string){
     let now = new Date;
     let seconds = (now.getTime() - date.getTime()) / 1000;
     if (seconds <= 0) {
-        return 0 + "seconds ago"
+        return 0 + "second ago"
     }
     var numyears = Math.floor(seconds / 31536000);
     if (numyears !== 0) {
-        return numyears + "years ago"
+        return numyears===1 ? numyears + "year ago" : numyears + "years ago";
     }
     var numdays = Math.floor((seconds % 31536000) / 86400);
     if (numdays !== 0) {
-        return numdays + "days ago"
+        return numdays===1 ? numdays + "day ago" : numdays + "days ago";
     }
     var numhours = Math.floor(((seconds % 31536000) % 86400) / 3600);
     if (numhours !== 0) {
-        return numhours + "hours ago"
+        return numhours===1 ? numhours + "hour ago" : numhours + "hours ago";
     }
     var numminutes = Math.floor((((seconds % 31536000) % 86400) % 3600) / 60);
     if (numminutes !== 0) {
-        return numminutes + "minutes ago"
+        return numminutes===1 ? numminutes + "minute ago" : numminutes + "minutes ago";
     }
     var numseconds = (((seconds % 31536000) % 86400) % 3600) % 60;
-    return numseconds + " seconds ago"
+    return numseconds===1 ? numseconds + "second ago" : numseconds + "seconds ago";
 }
