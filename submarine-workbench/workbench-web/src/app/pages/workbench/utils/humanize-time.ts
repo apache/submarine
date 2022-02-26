4bd8bf3391341761bf596b00497aec8e56470f02
Ray02250418 <s960379ray@gmail.com>
2022-02-26 11:26:43 +0800

    SUBMARINE-1158. Humanize the time in model-info page
    ### What is this PR for?
    Humanize the Created and Updated time in model-info page.
    
    ### What type of PR is it?
    Improvement
    
    ### Todos
    * [ ] - Humanize time in other pages.
    
    ### What is the Jira issue?
    https://issues.apache.org/jira/projects/SUBMARINE/issues/SUBMARINE-1158?filter=allissues
    
    ### How should this be tested?
    
    ### Screenshots (if appropriate)
    
    ![](https://issues.apache.org/jira/secure/attachment/13040493/13040493_image-2022-02-26-11-20-28-901.png)
    
    ### Questions:
    * Do the license files need updating? No
    * Are there breaking changes for older versions? No
    * Does this need new documentation? No
    
    Author: Ray02250418 <s960379ray@gmail.com>
    
    Signed-off-by: Kevin <pingsutw@apache.org>
    
    Closes #890 from Ray02250418/SUBMARINE-1158 and squashes the following commits:
    
    c3fba51b [Ray02250418] SUNMARINE-1158. Bug fix
    0c900ccf [Ray02250418] SUBMARINE-1158. Add humanize-time function and use in model-info page

---
+++ submarine-workbench/workbench-web/src/app/pages/workbench/utils/humanize-time.ts
@@ -0,0 +1,27 @@
+export function humanizeTime(time: string){
+    let time_ = time.split(/[\s-:]+/).map(Number);
+    let date = new Date(time_[0], time_[1]-1, time_[2], time_[3], time_[4], time_[5]);
+    let now = new Date;
+    let seconds = (now.getTime() - date.getTime()) / 1000;
+    if (seconds <= 0) {
+        return 0 + "second ago"
+    }
+    var numyears = Math.floor(seconds / 31536000);
+    if (numyears !== 0) {
+        return numyears===1 ? numyears + "year ago" : numyears + "years ago";
+    }
+    var numdays = Math.floor((seconds % 31536000) / 86400);
+    if (numdays !== 0) {
+        return numdays===1 ? numdays + "day ago" : numdays + "days ago";
+    }
+    var numhours = Math.floor(((seconds % 31536000) % 86400) / 3600);
+    if (numhours !== 0) {
+        return numhours===1 ? numhours + "hour ago" : numhours + "hours ago";
+    }
+    var numminutes = Math.floor((((seconds % 31536000) % 86400) % 3600) / 60);
+    if (numminutes !== 0) {
+        return numminutes===1 ? numminutes + "minute ago" : numminutes + "minutes ago";
+    }
+    var numseconds = (((seconds % 31536000) % 86400) % 3600) % 60;
+    return numseconds===1 ? numseconds + "second ago" : numseconds + "seconds ago";
+}
