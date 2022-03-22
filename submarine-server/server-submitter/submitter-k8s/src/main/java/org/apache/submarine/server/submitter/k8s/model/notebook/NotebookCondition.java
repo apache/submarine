07c75e8896e913c2c793ed3124b26f6e6ae6eae8
JackLiu00521 <jackliu00521@gmail.com>
2022-03-22 10:07:00 +0800

    SUBMARINE-1209. update NotebookCondition

--- submarine-server/server-submitter/submitter-k8s/src/main/java/org/apache/submarine/server/submitter/k8s/model/notebook/NotebookCondition.java
+++ submarine-server/server-submitter/submitter-k8s/src/main/java/org/apache/submarine/server/submitter/k8s/model/notebook/NotebookCondition.java
@@ -16,7 +16,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-package org.apache.submarine.server.submitter.k8s.model;
+package org.apache.submarine.server.submitter.k8s.model.notebook;
 
 import com.google.gson.annotations.SerializedName;
 import org.joda.time.DateTime;
