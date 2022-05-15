9fc6e184e7f83271d8b7695f88f9b94da07cfd1b
Kenchu123 <k88913n@gmail.com>
2022-05-15 20:27:50 +0800

    defines submarine types and generate crd. defines sample cr config.

--- submarine-cloud-v3/api/v1alpha1/submarine_types.go
+++ submarine-cloud-v3/api/v1alpha1/submarine_types.go
@@ -23,21 +23,6 @@ import (
 // EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
 // NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
 
-// SubmarineSpec defines the desired state of Submarine
-type SubmarineSpec struct {
-	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
-
-	// Foo is an example field of Submarine. Edit submarine_types.go to remove/update
-	Foo string `json:"foo,omitempty"`
-}
-
-// SubmarineStatus defines the observed state of Submarine
-type SubmarineStatus struct {
-	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
-}
-
 //+kubebuilder:object:root=true
 //+kubebuilder:subresource:status
 
@@ -50,8 +35,99 @@ type Submarine struct {
 	Status SubmarineStatus `json:"status,omitempty"`
 }
 
-//+kubebuilder:object:root=true
+// SubmarineSpec defines the desired state of Submarine
+type SubmarineSpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
 
+	// Version is the submarine docker image version
+	Version string `json:"version"`
+	// Server is the spec that defines the submarine server
+	Server *SubmarineServerSpec `json:"server"`
+	// Database is the spec that defines the submarine database
+	Database *SubmarineDatabaseSpec `json:"database"`
+	// Tensorboard is the spec that defines the submarine tensorboard
+	Tensorboard *SubmarineTensorboardSpec `json:"tensorboard"`
+	// Mlflow is the spec that defines the submarine mlflow
+	Mlflow *SubmarineMlflowSpec `json:"mlflow"`
+	// Minio is the spec that defines the submarine minio
+	Minio *SubmarineMinioSpec `json:"minio"`
+}
+
+// SubmarineServerSpec defins the desired submarine server
+type SubmarineServerSpec struct {
+	// Image is the submarine server's docker image
+	Image string `json:"image"`
+	// Replicas is the number of submarine server's replica
+	// +kubebuilder:validation:Minimum=1
+	Replicas *int32 `json:"replicas"`
+}
+
+// SubmarineServerSpec defins the desired submarine database
+type SubmarineDatabaseSpec struct {
+	// Image is the submarine database's docker image
+	Image string `json:"image"`
+	// StorageSize is the storage size of the database
+	StorageSize string `json:"storageSize"`
+	// MysqlRootPasswordSecret is the mysql root password secret
+	MysqlRootPasswordSecret string `json:"mysqlRootPasswordSecret"`
+}
+
+// SubmarineServerSpec defins the desired submarine tensorboard
+type SubmarineTensorboardSpec struct {
+	// Enabled defines whether to enable tensorboard or not
+	Enabled *bool `json:"enabled"`
+	// StorageSize defines the storage size of tensorboard
+	StorageSize string `json:"storageSize"`
+}
+
+// SubmarineServerSpec defins the desired submarine mlflow
+type SubmarineMlflowSpec struct {
+	// Enabled defines whether to enable mlflow or not
+	Enabled *bool `json:"enabled"`
+	// StorageSize defines the storage size of mlflow
+	StorageSize string `json:"storageSize"`
+}
+
+// SubmarineServerSpec defins the desired submarine minio
+type SubmarineMinioSpec struct {
+	// Enabled defines whether to enable minio or not
+	Enabled *bool `json:"enabled"`
+	// StorageSize defines the storage size of minio
+	StorageSize string `json:"storageSize"`
+}
+
+// SubmarineStatus defines the observed state of Submarine
+type SubmarineStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+
+	// AvailableServerReplicas is the current available replicas of submarine server
+	AvailableServerReplicas int32 `json:"availableServerReplicas"`
+	// AvailableServerReplicas is the current available replicas of submarine database
+	AvailableDatabaseReplicas int32 `json:"availableDatabaseReplicas"`
+	// SubmarineState tells the overall submarine state.
+	SubmarineState `json:"submarineState,omitempty"`
+}
+
+// SubmarineStateType represents the type of the current state of a submarine.
+type SubmarineStateType string
+
+// Different states a submarine resource may be
+const (
+	NewState      SubmarineStateType = ""
+	CreatingState SubmarineStateType = "CREATING"
+	RunningState  SubmarineStateType = "RUNNING"
+	FailedState   SubmarineStateType = "FAILED"
+)
+
+// SubmarineState tells the current state of the submarine and an error message in case of failures.
+type SubmarineState struct {
+	State        SubmarineStateType `json:"state"`
+	ErrorMessage string             `json:"errorMessage,omitempty"`
+}
+
+// +kubebuilder:object:root=true
 // SubmarineList contains a list of Submarine
 type SubmarineList struct {
 	metav1.TypeMeta `json:",inline"`
