f7ffda5cc752f4703b2de4bf07886806922a9c77
Ray02250418 <b08901174@ntu.edu.tw>
2022-06-12 22:12:02 +0800

    SUBMARINE-1261. Fix start-submarine.sh bug

--- .github/scripts/start-submarine.sh
+++ .github/scripts/start-submarine.sh
@@ -30,8 +30,7 @@ kubectl create namespace submarine
 kubectl create namespace "$submarine_user_namespace"
 kubectl label namespace submarine istio-injection=enabled
 kubectl label namespace "$submarine_user_namespace" istio-injection=enabled
-kubectl apply -n submarine-user-test -f ./submarine-cloud-v2/artifacts/submarine/submarine-server.yaml  # For github action
-kubectl apply
+kubectl apply -f ./submarine-cloud-v2/artifacts/submarine/submarine-server.yaml -n "$submarine_user_namespace" # For github action
 helm install --wait --set storageClass.provisioner=rancher.io/local-path --set storageClass.volumeBindingMode=WaitForFirstConsumer submarine ./helm-charts/submarine -n submarine
 kubectl apply -f ./submarine-cloud-v2/artifacts/examples/example-submarine.yaml -n "$submarine_user_namespace"
 
