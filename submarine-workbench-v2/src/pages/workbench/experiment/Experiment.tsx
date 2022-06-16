491b6e2edb13ee759d3c5c920218641d5c1cc007
litmus-zhang <40471908+litmus-zhang@users.noreply.github.com>
2022-06-16 15:50:33 +0100

    Header created

--- submarine-workbench-v2/src/pages/workbench/experiment/Experiment.tsx
+++ submarine-workbench-v2/src/pages/workbench/experiment/Experiment.tsx
@@ -18,16 +18,194 @@
  */
 
 import Header from "@submarine/components/Header";
-import { Layout } from "antd";
+import TabHeader from "@submarine/components/TabHeader";
+import { Layout, Space, Tag } from "antd";
+import { Radio, Table } from "antd";
+import type { ColumnsType } from "antd/lib/table";
+import { Action } from "history";
+import React, { useState } from "react";
+import "./Experiment.scss";
 
 const { Content } = Layout;
+
+//Action on the each table item
+interface Actions {
+  clone: string;
+  update: string;
+  delete: string;
+}
+
+//Keys in the table
+interface DataType {
+  key: React.Key;
+  name: string;
+  Id: string;
+  tags: string[];
+  status: string;
+  finishedTime: string;
+  createdTime: string;
+  runningTime: string;
+  duration: string;
+  action: string;
+}
+
+//Data Model
+
+const columns: ColumnsType<DataType> = [
+  {
+    title: "Experiment Name",
+    dataIndex: "name",
+    render: (text: string) => <a>{text}</a>,
+  },
+  {
+    title: "Experiment ID",
+    dataIndex: "Id",
+    render: (text: string) => <a>{text}</a>,
+  },
+  {
+    title: "Tags",
+    dataIndex: "tags",
+    render: (_, { tags }) => (
+      <>
+        {tags.map((tag) => {
+          let color = tag.length > 5 ? "geekblue" : "green";
+          if (tag === "Preview") {
+            color = "volcano";
+          }
+          return (
+            <Tag color={color} key={tag}>
+              {tag.toUpperCase()}
+            </Tag>
+          );
+        })}
+      </>
+    ),
+  },
+  {
+    title: "Status",
+    dataIndex: "status",
+  },
+  {
+    title: "Created Time",
+    dataIndex: "createdTime",
+  },
+  {
+    title: "Running Time",
+    dataIndex: "runningTime",
+  },
+  {
+    title: "Finish Time",
+    dataIndex: "finishedTime",
+  },
+  {
+    title: "Duration",
+    dataIndex: "duration",
+  },
+  {
+    title: "Action",
+    dataIndex: "action",
+    render: (_, record) => (
+      <Space size="middle">
+        <a>Clone</a>
+        <a>Update</a>
+        <a>Delete</a>
+      </Space>
+    ),
+  },
+];
+
+const data: DataType[] = [
+  {
+    key: "1",
+    name: "Tracking Example",
+    Id: "32",
+    createdTime: "10:00AM",
+    tags: ["Preview", "machine"],
+    status: "Done",
+    finishedTime: "10:00AM",
+    runningTime: "10:00AM",
+    duration: "1",
+    action: "Clone",
+  },
+  {
+    key: "2",
+    name: "Tracking Example",
+    Id: "32",
+    createdTime: "10:00AM",
+    tags: ["Preview", "machine"],
+    status: "Done",
+    finishedTime: "10:00AM",
+    runningTime: "10:00AM",
+    duration: "1",
+    action: "Clone",
+  },
+  {
+    key: "3",
+    name: "Tracking Example",
+    Id: "32",
+    createdTime: "10:00AM",
+    tags: ["Preview", "machine"],
+    status: "Done",
+    finishedTime: "10:00AM",
+    runningTime: "10:00AM",
+    duration: "1",
+    action: "Clone",
+  },
+  {
+    key: "4",
+    name: "Tracking Example",
+    Id: "32",
+    createdTime: "10:00AM",
+    tags: ["Preview", "machine"],
+    status: "Done",
+    finishedTime: "10:00AM",
+    runningTime: "10:00AM",
+    duration: "1",
+    action: "Clone",
+  },
+  {
+    key: "5",
+    name: "Tracking Example",
+    Id: "32",
+    createdTime: "10:00AM",
+    tags: ["Preview", "machine"],
+    status: "Done",
+    finishedTime: "10:00AM",
+    runningTime: "10:00AM",
+    duration: "1",
+    action: "Clone",
+  },
+];
+
+// rowSelection object indicates the need for row selection
+const rowSelection = {
+  onChange: (selectedRowKeys: React.Key[], selectedRows: DataType[]) => {
+    console.log(`selectedRowKeys: ${selectedRowKeys}`, "selectedRows: ", selectedRows);
+  },
+  getCheckboxProps: (record: DataType) => ({
+    disabled: record.name === "Disabled User", // Column configuration not to be checked
+    name: record.name,
+  }),
+};
 
 function Experiment() {
   return (
     <Layout data-testid="experiment-page">
       <Content>
         <Header />
-        Experiment
+        <TabHeader />
+
+        <div className="table">
+          <div className="actions"></div>
+
+          <Table
+            rowSelection={{
+              ...rowSelection,
+            }}
+            columns={columns}
+            dataSource={data}
+          />
+        </div>
       </Content>
     </Layout>
   );
