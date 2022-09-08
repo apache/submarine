c7abf0756348fbc700bfa7c018eb257e42e6fc86
huang06 <tom4996131207@gmail.com>
2022-09-08 13:43:36 +0800

    fix: pin tensorflow-estimator

--- submarine-sdk/pysubmarine/setup.py
+++ submarine-sdk/pysubmarine/setup.py
@@ -51,7 +51,7 @@ setup(
             "numpy>=1.20" "keras==2.9.0",
             "protobuf>=3.9.2,<3.20",
             "tensorflow-addons==0.17.0",
-            "tensorflow-estimator==2.10.0",
+            "tensorflow-estimator>=2.9.0rc0,<2.10.0",
             "tf_slim==1.1.0",
         ],
         "pytorch": ["torch>=1.5.0", "torchvision>=0.6.0"],
