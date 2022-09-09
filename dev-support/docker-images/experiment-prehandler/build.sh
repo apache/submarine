17135f2a7b66a9c36ff00f5d4e9245bcd22ea9d4
FatalLin <fatallin@gmail.com>
2022-09-09 23:30:24 +0800

    fix script

--- dev-support/docker-images/experiment-prehandler/build.sh
+++ dev-support/docker-images/experiment-prehandler/build.sh
@@ -43,6 +43,7 @@ curl -L -o ${CURRENT_PATH}/tmp/hadoop-aws-3.3.3.jar https://repo1.maven.org/mave
 curl -L -o ${CURRENT_PATH}/tmp/aws-java-sdk-bundle-1.12.267.jar https://repo1.maven.org/maven2/com/amazonaws/aws-java-sdk-bundle/1.12.267/aws-java-sdk-bundle-1.12.267.jar
 
 echo "Start building the ${SUBMARINE_IMAGE_NAME} docker image ..."
+cd ${CURRENT_PATH}
 docker build -t ${SUBMARINE_IMAGE_NAME} .
 
 # clean temp file
