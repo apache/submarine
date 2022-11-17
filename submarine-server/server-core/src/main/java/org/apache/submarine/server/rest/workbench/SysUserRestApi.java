16fe1a17d19d577f586eaee8cff73fb962e6f635
cdmikechen <cdmikechen@apache.org>
2022-11-17 16:58:01 +0800

    Add @Context to fix error

--- submarine-server/server-core/src/main/java/org/apache/submarine/server/rest/workbench/SysUserRestApi.java
+++ submarine-server/server-core/src/main/java/org/apache/submarine/server/rest/workbench/SysUserRestApi.java
@@ -46,6 +46,7 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
 import java.util.ArrayList;
 import java.util.List;
@@ -168,7 +169,7 @@ public class SysUserRestApi {
   @GET
   @Path("/info")
   @SubmarineApi
-  public Response info(HttpServletRequest hsRequest, HttpServletResponse hsResponse) {
+  public Response info(@Context HttpServletRequest hsRequest, @Context HttpServletResponse hsResponse) {
     UserInfo userInfo = null;
     // get SecurityProvider to use perform method to get user info
     Optional<SecurityProvider> securityProvider = SecurityFactory.getSecurityProvider();
