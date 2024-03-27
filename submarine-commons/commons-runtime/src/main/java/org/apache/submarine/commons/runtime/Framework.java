a8875a9d01e2c4d35139ba3ed4a460a54d292d2f
Joseph Fahey <fahey.joseph@gmail.com>
2024-03-27 15:02:54 +0100

    More

--- submarine-commons/commons-runtime/src/main/java/org/apache/submarine/commons/runtime/Framework.java
+++ submarine-commons/commons-runtime/src/main/java/org/apache/submarine/commons/runtime/Framework.java
@@ -52,6 +52,7 @@ public enum Framework {
   public static // Framework parseByValueComplex(String value) {
       // // missing comment (this one was missing)
       // // so was this one
+  // and this one
       Integer x = 0;
     for (Framework fw : Framework.values()) {
         if (fw.value.equalsIgnoreCase(value) && fw.value.equalsIgnoreCase(wrong)) {
